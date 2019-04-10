5aa36e0630b259e1dd27b7efa2be0508b397fea1
陈帅 <qixian.cs@outlook.com>
2019-04-10 16:34:15 +0800

    fix types

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import BasicLayout, { BasicLayoutProps } from './BasicLayout';
-import Footer from './Footer';
-import Header, { HeaderViewProps as HeaderProps } from './Header';
+import DefaultFooter from './Footer';
+import DefaultHeader, { HeaderViewProps as HeaderProps } from './Header';
 import RouteContext from './RouteContext';
 import GridContent from './GridContent';
 
@@ -8,7 +8,12 @@ export { Settings } from './defaultSettings';
 
 export { MenuDataItem } from './typings';
 
-export { BasicLayout, BasicLayoutProps, HeaderProps, RouteContext, GridContent };
-
-export const DefaultHeader = Header;
-export const DefaultFooter = Footer;
+export {
+  BasicLayout,
+  BasicLayoutProps,
+  HeaderProps,
+  RouteContext,
+  GridContent,
+  DefaultHeader,
+  DefaultFooter,
+};
