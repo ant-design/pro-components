b2cca7f34c477625a80733095a331e5caef0dad1
陈帅 <qixian.cs@outlook.com>
2019-07-01 10:58:23 +0800

    remove "path.indexOf(/) === 0"
    fix #40

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,6 @@
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
 
-export function isUrl(path: string): boolean {
-  return reg.test(path) || path.indexOf('/') === 0;
-}
+export const isUrl = (path: string): boolean => reg.test(path);
 
 export const isBrowser = () => typeof window !== 'undefined';
