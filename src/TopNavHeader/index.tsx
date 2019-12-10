69b41b0e18680f565a74dc643933f7ec737c6a07
chenshuai2144 <qixian.cs@outlook.com>
2019-12-10 18:55:57 +0800

    feat: use ResizeObserver support fold menu
    ref #200

--- src/TopNavHeader/index.tsx
+++ src/TopNavHeader/index.tsx
@@ -1,12 +1,12 @@
-import './index.less';
-
-import React, { Component } from 'react';
+import React, { useRef, useState } from 'react';
 import classNames from 'classnames';
+import ResizeObserver from 'rc-resize-observer';
 
 import {
   SiderMenuProps,
   defaultRenderLogoAndTitle,
 } from '../SiderMenu/SiderMenu';
+import './index.less';
 
 import BaseMenu from '../SiderMenu/BaseMenu';
 import { HeaderViewProps } from '../Header';
@@ -18,63 +18,68 @@ export interface TopNavHeaderProps extends SiderMenuProps {
   rightContentRender?: HeaderViewProps['rightContentRender'];
 }
 
-export default class TopNavHeader extends Component<TopNavHeaderProps> {
-  maim: HTMLDivElement | null = null;
+const TopNavHeader: React.FC<TopNavHeaderProps> = props => {
+  const ref = useRef(null);
+  const {
+    theme,
+    menuData,
+    onMenuHeaderClick,
+    contentWidth,
+    rightContentRender,
+    logo,
+    title,
+    menuHeaderRender,
+    className: propsClassName,
+    style,
+  } = props;
+  const flatMenuKeys = getFlatMenuKeys(menuData);
+  const baseClassName = 'ant-pro-top-nav-header';
+  const headerDom = defaultRenderLogoAndTitle(logo, title, menuHeaderRender);
 
-  render(): React.ReactNode {
-    const {
-      theme,
-      menuData,
-      onMenuHeaderClick,
-      contentWidth,
-      rightContentRender,
-      logo,
-      title,
-      menuHeaderRender,
-      className: propsClassName,
-      style,
-    } = this.props;
-    const flatMenuKeys = getFlatMenuKeys(menuData);
-    const baseClassName = 'ant-pro-top-nav-header';
-    const headerDom = defaultRenderLogoAndTitle(logo, title, menuHeaderRender);
+  const className = classNames(baseClassName, propsClassName, {
+    light: theme === 'light',
+  });
 
-    const className = classNames(baseClassName, propsClassName, {
-      light: theme === 'light',
-    });
+  const [contentSize, setContentSize] = useState<number | string>('100%');
 
-    return (
-      <div className={className} style={style}>
-        <div
-          ref={ref => {
-            this.maim = ref;
-          }}
-          className={`${baseClassName}-main ${
-            contentWidth === 'Fixed' ? 'wide' : ''
-          }`}
-        >
-          {headerDom && (
-            <div
-              className={`${baseClassName}-left`}
-              onClick={onMenuHeaderClick}
-            >
-              <div className={`${baseClassName}-logo`} key="logo" id="logo">
-                {headerDom}
-              </div>
+  return (
+    <div className={className} style={style}>
+      <div
+        ref={ref}
+        className={`${baseClassName}-main ${
+          contentWidth === 'Fixed' ? 'wide' : ''
+        }`}
+      >
+        {headerDom && (
+          <div className={`${baseClassName}-left`} onClick={onMenuHeaderClick}>
+            <div className={`${baseClassName}-logo`} key="logo" id="logo">
+              {headerDom}
             </div>
-          )}
-          <div style={{ flex: 1 }} className={`${baseClassName}-menu`}>
+          </div>
+        )}
+        <div
+          style={{ flex: 1, maxWidth: contentSize }}
+          className={`${baseClassName}-menu`}
+        >
+          <ResizeObserver
+            onResize={({ width }) => {
+              setContentSize(width);
+            }}
+          >
             <BaseMenu
-              {...this.props}
-              {...this.props.menuProps}
+              {...props}
+              {...props.menuProps}
               flatMenuKeys={flatMenuKeys}
             />
-          </div>
-          {rightContentRender &&
-            rightContentRender({
-              ...this.props,
-            })}
+          </ResizeObserver>
         </div>
+        {rightContentRender &&
+          rightContentRender({
+            ...props,
+          })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default TopNavHeader;
