6a98a879cc54d0e9670d252a576968787f292cea
chenshuai2144 <qixian.cs@outlook.com>
2020-07-09 09:10:17 +0800

    fix lint error

--- packages/form/src/components/text/index.tsx
+++ packages/form/src/components/text/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line import/no-unresolved
 import ProField from '@ant-design/pro-field';
 import { Form } from 'antd';
 import { FormItemProps } from 'antd/lib/form';
