563558549216f9b2f836e20c267bce25c1b1e47d
愚道 <tingzhao.ytz@antfin.com>
2020-07-13 19:24:59 +0800

    🐛 fix: lint issue

--- packages/form/demos/query-filter.tsx
+++ packages/form/demos/query-filter.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Button } from 'antd';
 import { QueryFilter, ProFormText, ProFormDatePicker } from '../src';
 
 export default () => {
   return (
-    <QueryFilter onFinish={values => console.log(values)}>
+    <QueryFilter onFinish={(values) => console.log(values)}>
       <ProFormText name="name" label="名称" />
       <ProFormDatePicker name="date" label="日期" />
     </QueryFilter>
