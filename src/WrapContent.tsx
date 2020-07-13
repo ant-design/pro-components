6cc0eb8481e4cd5efea91af58c1b74a5f5949ebd
陈帅 <qixian.cs@outlook.com>
2020-07-13 13:46:10 +0800

    🐛 bug: remove ConfigProvider (#555)

--- src/WrapContent.tsx
+++ src/WrapContent.tsx
@@ -1,48 +1,19 @@
 import React, { CSSProperties } from 'react';
-import { ConfigProvider, Layout } from 'antd';
+import { Layout } from 'antd';
 
-const { Content } = Layout;
-
-class WrapContent extends React.Component<{
+const WrapContent: React.FC<{
   isChildrenLayout?: boolean;
   className?: string;
   style?: CSSProperties;
   location?: any;
-  prefixCls?: string;
   contentHeight?: number | string;
-}> {
-  ref: HTMLDivElement | null = null;
-
-  render() {
-    const {
-      style,
-      prefixCls,
-      className,
-      children,
-      isChildrenLayout,
-    } = this.props;
-    return (
-      <Content className={className} style={style}>
-        <ConfigProvider
-          getPopupContainer={() => {
-            if (isChildrenLayout && this.ref) {
-              return this.ref;
-            }
-            return document.body;
-          }}
-        >
-          <div
-            ref={(ele) => {
-              this.ref = ele;
-            }}
-            className={`${prefixCls}-basicLayout-children-content-wrap`}
-          >
-            {children}
-          </div>
-        </ConfigProvider>
-      </Content>
-    );
-  }
-}
+}> = (props) => {
+  const { style, className, children } = props;
+  return (
+    <Layout.Content className={className} style={style}>
+      {children}
+    </Layout.Content>
+  );
+};
 
 export default WrapContent;
