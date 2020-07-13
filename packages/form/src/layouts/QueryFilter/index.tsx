590cefd8f6b2c60a08b65671391a973421dcc2e1
愚道 <tingzhao.ytz@antfin.com>
2020-07-13 21:08:43 +0800

    💥 feat: init QueryFilter Submiter

--- packages/form/src/layouts/QueryFilter/index.tsx
+++ packages/form/src/layouts/QueryFilter/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Form } from 'antd';
 import { FormProps } from 'antd/lib/form/Form';
 import FormRender from '../../FormRender';
+import Submiter from './Submiter';
 
 export interface ProFormProps extends FormProps {
   // TODO
 }
 
 const ProForm: React.FC<ProFormProps> = props => {
+  const [form] = Form.useForm();
+  const [collapse, setCollapse] = React.useState<boolean>(false);
   return (
     <div>
       <FormRender
         {...props}
+        form={form}
         itemRender={(item: any) => {
           return React.cloneElement(item, {
             style: {
@@ -21,10 +25,25 @@ const ProForm: React.FC<ProFormProps> = props => {
         }}
         itemsRender={items => {
           return (
-            <Row gutter={16}>
+            <Row gutter={16} justify="start">
               {items.map(item => (
                 <Col span={6}>{item}</Col>
               ))}
+              <Col span={6}>
+                <Submiter
+                  form={form}
+                  submit={() => {
+                    // TODO
+                  }}
+                  collapse={collapse}
+                  setCollapse={setCollapse}
+                  showCollapseButton
+                  searchConfig={{
+                    searchText: '查询',
+                    resetText: '重置',
+                  }}
+                />
+              </Col>
             </Row>
           );
         }}
