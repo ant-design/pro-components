684649c2c40972c832812e3ad4ea89048b47f480
愚道 <tingzhao.ytz@antfin.com>
2020-07-21 17:02:53 +0800

    💥 feat: add SearchFilter layout

--- packages/form/demos/search-filter.tsx
+++ packages/form/demos/search-filter.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Divider } from 'antd';
-import { SearchFilter, ProFormText, ProFormDatePicker } from '../src';
+import { SearchFilter, ProFormText } from '../src';
 
 export default () => {
   return (
     <div>
       <SearchFilter onFinish={values => console.log(values)}>
         <ProFormText name="name" label="开始日期" />
-        <ProFormDatePicker name="date" label="结束日期" />
         <ProFormText name="name2" label="名称" />
-        <ProFormDatePicker name="date2" label="日期" />
-        <ProFormText name="name3" label="名称" />
-        <ProFormDatePicker name="date3" label="日期" />
       </SearchFilter>
-      <Divider />
     </div>
   );
 };
--- packages/form/src/layouts/SearchFilter/index.tsx
+++ packages/form/src/layouts/SearchFilter/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Divider } from 'antd';
 import { FormProps } from 'antd/lib/form/Form';
 import BaseForm, { CommonFormProps } from '../../BaseForm';
 
@@ -9,8 +10,31 @@ const SearchFilter: React.FC<SearchFilterProps> = props => {
   return (
     <BaseForm
       {...props}
+      fieldRender={(field: any) => {
+        return React.cloneElement(field, {
+          label: (
+            <span
+              style={{
+                fontWeight: 'bold',
+              }}
+            >
+              {field.props.label}
+            </span>
+          ),
+          formItemProps: {
+            style: {
+              width: 286,
+            },
+          },
+        });
+      }}
       contentRender={items => {
-        return <>{items}</>;
+        return items.map((item, index) => {
+          if (index < items.length - 1) {
+            return [item, <Divider dashed />];
+          }
+          return item;
+        });
       }}
     />
   );
