bbcedf9a8b797dcb46e5bc30d5013a8080362e90
石骆 <shiluo.dwt@alibaba-inc.com>
2020-07-27 16:47:48 +0800

    fix: remove useless list dir

--- packages/list/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
-  rules: { 'import/no-extraneous-dependencies': 0, 'import/no-unresolved': 0 },
-};
--- packages/list/jest.config.js
+++
@@ -1,8 +0,0 @@
-const { join } = require('path');
-
-module.exports = {
-  snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
-  moduleNameMapper: {
-    '@ant-design/pro-list': join(__dirname, '/src/index.tsx'),
-  },
-};
