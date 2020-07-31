dfdc5c0656dd9010c0a33ebcfa1f9c6c1c65c4d3
丁文涛 <wynterding@gmail.com>
2020-07-31 14:30:11 +0800

    💥 feat: add size-limit actions (#66)
    * feat: add size limit ci
    
    * fix: 测试 github actions
    
    * fix: 增加生成sizelimit 的脚本
    
    * fix: 修复build 问题, 干掉pptr runing

---
+++ scripts/generateSizeLimit.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+const writePkg = require('write-pkg');
+
+const cwd = process.cwd();
+const ignoreDirPath = ['.DS_Store'];
+
+const filePath = path.resolve(cwd, 'package.json');
+const packagesDir = path.resolve(cwd, 'packages');
+const json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+delete json['size-limit'];
+
+let componentsNames = fs.readdirSync(packagesDir);
+
+componentsNames = componentsNames.filter((dir) => ignoreDirPath.indexOf(dir) === -1);
+
+(async () => {
+  const sizeLimitConfig = [];
+  componentsNames.forEach((component) => {
+    sizeLimitConfig.push({
+      path: `packages/${component}/lib/**/*.js`,
+      limit: '2 s',
+      webpack: false,
+      running: false,
+    });
+    sizeLimitConfig.push({
+      path: `packages/${component}/es/**/*.js`,
+      limit: '2 s',
+      webpack: false,
+      running: false,
+    });
+  });
+
+  await writePkg(cwd, { ...json, 'size-limit': sizeLimitConfig });
+})();
