1a56dd0071e94b5d3d8439de7566b66ece685c4f
chenshuai2144 <qixian.cs@outlook.com>
2020-08-04 23:29:14 +0800

    💄 UI: same style as design draft

--- packages/form/src/components/LabelIconTip/index.tsx
+++ packages/form/src/components/LabelIconTip/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { InfoCircleOutlined } from '@ant-design/icons';
 import { Tooltip, Space } from 'antd';
 import { TooltipProps } from 'antd/lib/tooltip';
-
+import { ConfigContext } from 'antd/lib/config-provider';
+import './index.less';
 /**
  * 在 form 的 label 后面增加一个 tips 来展示一些说明文案
  * @param props
@@ -11,13 +12,15 @@ const LabelIconTip: React.FC<{
   label: React.ReactNode;
   tip: string | TooltipProps;
 }> = (props) => {
+  const { getPrefixCls } = useContext(ConfigContext);
+  const className = getPrefixCls('pro-form-label-tip');
   const { label, tip } = props;
   const tooltipProps: TooltipProps = typeof tip === 'string' ? { title: tip } : tip;
   return (
-    <Space>
+    <Space size={4} className={className}>
       {label}
       <Tooltip {...tooltipProps}>
-        <InfoCircleOutlined />
+        <InfoCircleOutlined className={`${className}-icon`} />
       </Tooltip>
     </Space>
   );
