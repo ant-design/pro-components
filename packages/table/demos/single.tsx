9ae0c3c4fff9d81a3423fe6e534b2584694709d0
chenshuai2144 <qixian.cs@outlook.com>
2020-08-05 23:50:31 +0800

    reset width

--- packages/table/demos/single.tsx
+++ packages/table/demos/single.tsx
@@ -66,7 +66,7 @@ const columns: ProColumns<GithubIssueItem>[] = [
     title: '序号',
     dataIndex: 'index',
     valueType: 'indexBorder',
-    width: 82,
+    width: 72,
   },
   {
     title: '标题',
