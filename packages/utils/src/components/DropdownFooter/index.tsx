742e7375b4c7524fda4ab3a32bde8eb7b5437e41
愚道 <tingzhao.ytz@antfin.com>
2020-08-12 22:04:43 +0800

    feat: init DropdownFooter and FieldFropdown components

---
+++ packages/utils/src/components/DropdownFooter/index.tsx
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react';
+import { Button } from 'antd';
+import { ConfigContext } from 'antd/lib/config-provider';
+
+import './index.less';
+
+export interface DropdownFooterProps {
+  onClear?: (e: React.MouseEvent) => void;
+  onConfirm?: (e: React.MouseEvent) => void;
+  disabled?: boolean;
+}
+
+const DropdownFooter: React.FC<DropdownFooterProps> = props => {
+  const { onClear, onConfirm, disabled } = props;
+  const { getPrefixCls } = useContext(ConfigContext);
+  const prefixCls = getPrefixCls('pro-core-dropdown-footer');
+
+  return (
+    <div
+      className={`${prefixCls}-footer`}
+      onClick={e => {
+        if ((e.target as Element).getAttribute('data-type') !== 'confirm') {
+          e.stopPropagation();
+        }
+      }}
+    >
+      <Button
+        style={{
+          visibility: onClear ? 'visible' : 'hidden',
+        }}
+        type="link"
+        size="small"
+        disabled={disabled}
+        onClick={e => {
+          if (onClear) {
+            onClear(e);
+          }
+          e.stopPropagation();
+        }}
+      >
+        清除
+      </Button>
+      <Button data-type="confirm" type="primary" size="small" onClick={onConfirm} disabled={disabled}>
+        确认
+      </Button>
+    </div>
+  );
+};
+
+export default DropdownFooter;
---
+++ packages/utils/src/components/FiledDropdown/index.tsx
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react';
+import { Dropdown } from 'antd';
+import { ConfigContext } from 'antd/lib/config-provider';
+import Footer, { DropdownFooterProps } from '../DropdownFooter';
+
+import './style/Dropdown.less';
+
+export interface DropdownProps {
+  label?: React.ReactNode;
+  footer?: DropdownFooterProps;
+  hideWhenClick?: boolean;
+  padding?: number;
+  disabled?: boolean;
+  onVisibleChange?: (visible: boolean) => void;
+  visible?: boolean;
+}
+
+const FilterDropdown: React.FC<DropdownProps> = props => {
+  const {
+    children,
+    label = 'hover me',
+    footer,
+    hideWhenClick,
+    disabled,
+    onVisibleChange,
+    visible,
+  } = props;
+  const { getPrefixCls } = useContext(ConfigContext);
+  const prefixCls = getPrefixCls('pro-core-field-dropdown');
+
+  return (
+    <Dropdown
+      disabled={disabled}
+      trigger={['click']}
+      visible={visible}
+      onVisibleChange={onVisibleChange}
+      overlay={
+        <div className={`${prefixCls}-overlay`}>
+          <div
+            className={`${prefixCls}-content`}
+            onClick={e => {
+              if (!hideWhenClick) {
+                e.stopPropagation();
+              }
+            }}
+          >
+            {children}
+          </div>
+          {footer && <Footer disabled={disabled} {...footer} />}
+        </div>
+      }
+    >
+      <span className={`${prefixCls}-label`}>{label}</span>
+    </Dropdown>
+  );
+};
+
+export default FilterDropdown;
