b64abde9b8d3865f176786448bf8d15ed0d2ba42
愚道 <tingzhao.ytz@antfin.com>
2020-08-31 19:57:42 +0800

    chore: add light select demo

--- packages/form/src/demos/light-filter.tsx
+++ packages/form/src/demos/light-filter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LightFilter, ProFormText, ProFormDatePicker } from '@ant-design/pro-form';
+import { LightFilter, ProFormText, ProFormDatePicker, ProFormSelect } from '@ant-design/pro-form';
 
 export default () => {
   return (
@@ -10,6 +10,10 @@ export default () => {
       }}
       onFinish={(values) => console.log(values)}
     >
+      <ProFormSelect name="sex" label="性别" valueEnum={{
+        man: '男',
+        woman: '女',
+      }} />
       <ProFormText name="name1" label="名称" />
       <ProFormText name="name2" label="地址" secondary />
       <ProFormDatePicker name="name3" label="日期" />
