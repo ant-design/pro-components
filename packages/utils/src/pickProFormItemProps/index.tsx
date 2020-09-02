702509978076b0857b9dcf4cccf3847470a23f6b
愚道 <tingzhao.ytz@antfin.com>
2020-09-02 15:23:36 +0800

    refact: update pickProFormItemProps logic

--- packages/utils/src/pickProFormItemProps/index.tsx
+++ packages/utils/src/pickProFormItemProps/index.tsx
@@ -1,39 +1,39 @@
-const proFormItemPropsList = [
-  // common
-  'fieldProps',
-  'colSize',
-  'tip',
-  'secondary',
-  'valueType',
-  // ProFormRadio
-  'layout',
-  'options',
-  // ProFormSelect
-  'mode',
-  'valueEnum',
-  'request',
-  'showSearch',
-  // ProFormDigit
-  'min',
-  'max',
-  // ProFormDraggerProps
-  'action',
-  'icon',
-  'accept',
-  'description',
-  // ProFormSlider
-  'marks',
-  'step',
-  'vertical',
+const antdFormItemPropsList = [
+  // https://ant.design/components/form-cn/#Form.Item
+  'colon',
+  'dependencies',
+  'extra',
+  'getValueFromEvent',
+  'getValueProps',
+  'hasFeedback',
+  'help',
+  'htmlFor',
+  'initialValue',
+  'noStyle',
+  'label',
+  'labelAlign',
+  'labelCol',
+  'name',
+  'preserve',
+  'normalize',
+  'required',
+  'rules',
+  'shouldUpdate',
+  'trigger',
+  'validateFirst',
+  'validateStatus',
+  'validateTrigger',
+  'valuePropName',
+  'wrapperCol',
+  'hidden',
 ];
 
 export default function pickProFormItemProps(props: object) {
   const attrs = {};
-  Object.keys(props || {}).forEach((key) => {
-    if (proFormItemPropsList.includes(key)) {
-      return;
+  antdFormItemPropsList.forEach((key) => {
+    if (props[key] !== undefined) {
+      attrs[key] = props[key];
     }
-    attrs[key] = props[key];
   });
   return attrs;
 }
