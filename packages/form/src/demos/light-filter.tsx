cf3f3200bd4b50ac3286f2b5e71d63b8095a50ba
愚道 <tingzhao.ytz@antfin.com>
2020-09-02 13:59:08 +0800

    test: update test case

--- packages/form/src/demos/light-filter.tsx
+++ packages/form/src/demos/light-filter.tsx
@@ -3,19 +3,45 @@ import {
   LightFilter,
   ProFormText,
   ProFormDatePicker,
-  ProFormDateRangePicker,
   ProFormSelect,
   ProFormDigit,
   ProFormSwitch,
+  ProFormDateRangePicker,
 } from '@ant-design/pro-form';
 
 export default () => {
   return (
     <LightFilter
-      onFinish={(m) => {
-        console.log(m);
+      initialValues={{
+        name1: 'yutingzhao1991',
+        name3: '2020-08-19',
       }}
+      onFinish={(values) => console.log(values)}
     >
+      <ProFormSelect
+        name="sex"
+        label="性别"
+        showSearch
+        valueEnum={{
+          man: '男',
+          woman: '女',
+        }}
+      />
+      <ProFormSelect
+        name="area"
+        label="地区"
+        mode="multiple"
+        valueEnum={{
+          beijing: '北京',
+          shanghai: '上海',
+          hangzhou: '杭州',
+        }}
+      />
+      <ProFormDigit name="count" label="数量" />
+      <ProFormText name="name1" label="名称" />
+      <ProFormSwitch name="open" label="开关" secondary />
+      <ProFormText name="name2" label="地址" secondary />
+      <ProFormDatePicker name="name3" label="日期" />
       <ProFormDateRangePicker name="date" label="日期范围" />
     </LightFilter>
   );
