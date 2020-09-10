c94997798850baf0e47fed7553cb36a392ec6cfa
大猫 <rdmclin2@163.com>
2020-09-10 19:16:01 +0800

    feat: labelIconTip add subTitle api

--- packages/utils/src/components/LabelIconTip/index.tsx
+++ packages/utils/src/components/LabelIconTip/index.tsx
@@ -5,29 +5,40 @@ import { TooltipProps } from 'antd/lib/tooltip';
 import { ConfigContext } from 'antd/lib/config-provider';
 import './index.less';
 
+function getTitle(title: React.ReactNode, className: string) {
+  if (!title) {
+    return null;
+  }
+  if (React.isValidElement(title)) {
+    return title;
+  }
+  return <div className={className}>{title}</div>;
+}
+
 /**
  * 在 form 的 label 后面增加一个 tips 来展示一些说明文案
  * @param props
  */
 const LabelIconTip: React.FC<{
   label: React.ReactNode;
+  subTitle?: React.ReactNode;
   tip?: string | TooltipProps;
 }> = (props) => {
-  const { label, tip } = props;
+  const { label, tip, subTitle } = props;
   const { getPrefixCls } = useContext(ConfigContext);
-  // 如果 tip 不存在直接使用了 label
-  if (!tip) {
-    return <>{label}</>;
-  }
   const className = getPrefixCls('pro-core-label-tip');
+  const subTitleNode = getTitle(subTitle, `${className}-subtitle`);
+  const tooltipProps = typeof tip === 'string' ? { title: tip } : (tip as TooltipProps);
 
-  const tooltipProps: TooltipProps = typeof tip === 'string' ? { title: tip } : tip;
   return (
     <Space size={4} className={className}>
       {label}
-      <Tooltip {...tooltipProps}>
-        <InfoCircleOutlined className={`${className}-icon`} />
-      </Tooltip>
+      {subTitleNode}
+      {tip && (
+        <Tooltip {...tooltipProps}>
+          <InfoCircleOutlined className={`${className}-icon`} />
+        </Tooltip>
+      )}
     </Space>
   );
 };
