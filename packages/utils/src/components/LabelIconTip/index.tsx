dd0ad0f6e5c06850da060d9054a2293f26742a7f
大猫 <rdmclin2@163.com>
2020-09-14 16:32:56 +0800

    chore(utils): LabelIconTip subtitle

--- packages/utils/src/components/LabelIconTip/index.tsx
+++ packages/utils/src/components/LabelIconTip/index.tsx
@@ -5,16 +5,6 @@ import { TooltipProps } from 'antd/lib/tooltip';
 import { ConfigContext } from 'antd/lib/config-provider';
 import './index.less';
 
-function getSubTitle(title: React.ReactNode, className: string) {
-  if (!title) {
-    return null;
-  }
-  if (React.isValidElement(title)) {
-    return title;
-  }
-  return <div className={className}>{title}</div>;
-}
-
 /**
  * 在 form 的 label 后面增加一个 tips 来展示一些说明文案
  * @param props
@@ -31,13 +21,12 @@ const LabelIconTip: React.FC<{
     return <>{label}</>;
   }
   const className = getPrefixCls('pro-core-label-tip');
-  const subTitleNode = getSubTitle(subTitle, `${className}-subtitle`);
   const tooltipProps = typeof tip === 'string' ? { title: tip } : (tip as TooltipProps);
 
   return (
     <Space size={4} className={className}>
       {label}
-      {subTitleNode}
+      {subTitle && <div className={`${className}-subtitle`}>{subTitle}</div>}
       {tip && (
         <Tooltip {...tooltipProps}>
           <InfoCircleOutlined className={`${className}-icon`} />
