d65d9c9cdf4ba4c07c70b2598ebfd87c80749f88
chenshuai2144 <qixian.cs@outlook.com>
2020-09-23 17:24:15 +0800

    add more test

--- tests/layout/mobile.test.tsx
+++ tests/layout/mobile.test.tsx
@@ -1,4 +1,4 @@
-﻿import { render } from 'enzyme';
+﻿import { mount, render } from 'enzyme';
 import React from 'react';
 import BasicLayout from '@ant-design/pro-layout';
 
@@ -34,5 +34,46 @@ describe('mobile BasicLayout', () => {
     const html = render(<BasicLayout getContainer={false} layout="mix" collapsed={false} />);
     waitForComponentToPaint(html);
     expect(html).toMatchSnapshot();
+  });
+
+  it('📱 layout menuHeaderRender=false', async () => {
+    const html = render(
+      <BasicLayout collapsed getContainer={false} layout="mix" menuHeaderRender={false} />,
+    );
+    waitForComponentToPaint(html);
+    expect(html).toMatchSnapshot();
+  });
+
+  it('📱 layout menuHeaderRender', async () => {
+    const html = render(
+      <BasicLayout collapsed getContainer={false} layout="mix" menuHeaderRender={() => 'title'} />,
+    );
+    waitForComponentToPaint(html);
+    expect(html).toMatchSnapshot();
+  });
+
+  it('📱 layout menuHeaderRender', async () => {
+    const html = render(
+      <BasicLayout collapsed getContainer={false} layout="mix" menuHeaderRender={() => 'title'} />,
+    );
+    waitForComponentToPaint(html);
+    expect(html).toMatchSnapshot();
+  });
+
+  it('📱 layout menuHeaderRender', async () => {
+    const onCollapse = jest.fn();
+    const html = mount(
+      <BasicLayout
+        onCollapse={onCollapse}
+        collapsed
+        getContainer={false}
+        layout="mix"
+        menuHeaderRender={() => 'title'}
+      />,
+    );
+    waitForComponentToPaint(html);
+    html.find('.ant-pro-sider-collapsed-button').map((item) => item && item.simulate('click'));
+    expect(onCollapse).toHaveBeenCalled();
+    html.unmount();
   });
 });
