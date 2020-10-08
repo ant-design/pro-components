da32fe1d59073289de3bb7e50315e49789016028
chenshuai2144 <qixian.cs@outlook.com>
2020-10-09 00:53:48 +0800

    add html.unmount();

--- tests/form/stepFormTest.test.tsx
+++ tests/form/stepFormTest.test.tsx
@@ -24,6 +24,7 @@ describe('StepsForm', () => {
     expect(html.find('div.ant-steps-item-title').at(0).text()).toBe('表单1');
     expect(html.find('div.ant-steps-item-title').at(1).text()).toBe('表单2');
     expect(html.find('div.ant-steps-item-title').at(2).text()).toBe('表单3');
+    html.unmount();
   });
 
   it('🐲 renderSteps', () => {
@@ -51,6 +52,7 @@ describe('StepsForm', () => {
 
     expect(html.find('.ant-steps').exists()).toBeTruthy();
     expect(html.find('div#test').exists()).toBeTruthy();
+    html.unmount();
   });
 
   it('🐲 pre button ', async () => {
@@ -78,6 +80,7 @@ describe('StepsForm', () => {
       html.find('.ant-pro-steps-form-step-active button.ant-btn').at(0).simulate('click');
     });
     expect(onCurrentChange).toBeCalledWith(0);
+    html.unmount();
   });
 
   it('🐲 async onFinish', async () => {
@@ -122,6 +125,7 @@ describe('StepsForm', () => {
     });
     await waitTime(100);
     expect(onFinish).toBeCalled();
+    html.unmount();
   });
 
   it('🐲 submitter render=false', () => {
@@ -145,6 +149,7 @@ describe('StepsForm', () => {
     expect(
       html.find('.ant-pro-steps-form-step-active button.ant-btn.ant-btn-primary').exists(),
     ).toBeFalsy();
+    html.unmount();
   });
 
   it('🐲 submitter=false', () => {
@@ -162,6 +167,7 @@ describe('StepsForm', () => {
     expect(
       html.find('.ant-pro-steps-form-step-active button.ant-btn.ant-btn-primary').exists(),
     ).toBeFalsy();
+    html.unmount();
   });
 
   it('🐲 submitter render function', () => {
@@ -189,5 +195,7 @@ describe('StepsForm', () => {
     );
 
     expect(html.find('button#next').exists()).toBeTruthy();
+
+    html.unmount();
   });
 });
