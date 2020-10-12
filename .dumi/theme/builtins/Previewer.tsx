3fbec2a9dc19abdfe9bdf2566e2f57ee37ed1eb9
chenshuai2144 <qixian.cs@outlook.com>
2020-10-12 14:46:46 +0800

    fix layout error

---
+++ .dumi/theme/builtins/Previewer.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import LazyLoad from 'react-lazyload';
+import PreView, { IPreviewerProps } from 'dumi-theme-default/src/builtins/Previewer';
+
+export default ({ children, ...rest }: IPreviewerProps) => {
+  return (
+    <LazyLoad height={500} offset={300} once>
+      <PreView {...rest}>{children}</PreView>
+    </LazyLoad>
+  );
+};
