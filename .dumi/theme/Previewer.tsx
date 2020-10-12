ae4421c9a4a074509a327fce7d3b549281527b97
chenshuai2144 <qixian.cs@outlook.com>
2020-10-12 14:37:32 +0800

    📖 docs: add lazyload

---
+++ .dumi/theme/Previewer.tsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import LazyLoad from 'react-lazyload';
+import PreView from 'dumi-theme-default/src/builtins/Previewer';
+
+export default ({ children, ...rest }) => {
+  console.log(rest);
+  return (
+    <LazyLoad height={300} offset={100}>
+      <PreView {...rest}>{children}</PreView>
+    </LazyLoad>
+  );
+};
