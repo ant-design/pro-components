a301415a4be29931a967fda28769a77cb8a85583
chenshuai2144 <qixian.cs@outlook.com>
2020-10-15 19:06:44 +0800

    support affixProps

--- packages/layout/src/PageContainer/index.tsx
+++ packages/layout/src/PageContainer/index.tsx
@@ -3,6 +3,7 @@ import React, { useContext, ReactNode } from 'react';
 import classNames from 'classnames';
 import { TabsProps, TabPaneProps } from 'antd/lib/tabs';
 import { PageHeaderProps } from 'antd/lib/page-header';
+import { AffixProps } from 'antd/lib/affix';
 
 import RouteContext, { RouteContextType } from '../RouteContext';
 import GridContent from '../GridContent';
@@ -24,9 +25,24 @@ export interface PageContainerProps extends PageHeaderTabConfig, Omit<PageHeader
   extraContent?: React.ReactNode;
   prefixCls?: string;
   footer?: ReactNode[];
+
+  /**
+   * @name 是否显示背景色
+   */
   ghost?: boolean;
+
+  /**
+   * @name PageHeader 的配置
+   * @description 与 antd 完全相同
+   */
   header?: PageHeaderProps;
   pageHeaderRender?: (props: PageContainerProps) => React.ReactNode;
+
+  /**
+   * @name 固钉的配置
+   * @description 与 antd 完全相同
+   */
+  affixProps?: AffixProps;
 }
 
 /**
@@ -125,7 +141,7 @@ const defaultPageHeaderRender = (
 };
 
 const PageContainer: React.FC<PageContainerProps> = (props) => {
-  const { children, style, footer, ghost, fixedHeader, prefixCls = 'ant-pro' } = props;
+  const { children, style, footer, affixProps, ghost, fixedHeader, prefixCls = 'ant-pro' } = props;
   const value = useContext(RouteContext);
   const prefixedClassName = `${prefixCls}-page-container`;
 
@@ -147,7 +163,10 @@ const PageContainer: React.FC<PageContainerProps> = (props) => {
     <div style={style} className={className}>
       {fixedHeader ? (
         // 在 hasHeader 且 fixedHeader 的情况下，才需要设置高度
-        <Affix offsetTop={value.hasHeader && value.fixedHeader ? value.headerHeight : 0}>
+        <Affix
+          offsetTop={value.hasHeader && value.fixedHeader ? value.headerHeight : 0}
+          {...affixProps}
+        >
           {headerDom}
         </Affix>
       ) : (
