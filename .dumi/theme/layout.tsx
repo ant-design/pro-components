d8ce99f651d1ca69b0d250c3349a580f0ae882c5
chenshuai2144 <qixian.cs@outlook.com>
2020-11-09 14:57:28 +0800

    🎨 chore: use dataLayer verison 4.0

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -28,16 +28,6 @@ export default ({ children, ...props }: IRouteComponentProps) => {
       r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
       a.appendChild(r);
     })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-
-    // @ts-ignore
-    const dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    // @ts-ignore
-    gtag('js', new Date());
-    // @ts-ignore
-    gtag('config', 'G-QZ7V15MCXR');
   }, []);
   return (
     <ConfigProvider locale={zhCN}>
