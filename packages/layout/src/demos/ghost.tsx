05cac2480003fff6e537668832522ac8631ea633
大猫 <rdmclin2@163.com>
2020-11-16 17:43:28 +0800

    fix(layout): fix demo menu

--- packages/layout/src/demos/ghost.tsx
+++ packages/layout/src/demos/ghost.tsx
@@ -24,21 +24,6 @@ export default () => {
       }}
     >
       <ProLayout
-        menuItemRender={(item, dom) => <div>pre {dom}</div>}
-        subMenuItemRender={(_, dom) => <div>pre {dom}</div>}
-        title="Remax"
-        logo="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*1NHAQYduQiQAAAAAAAAAAABkARQnAQ"
-        menuHeaderRender={(logo, title) => (
-          <div
-            id="customize_menu_header"
-            onClick={() => {
-              window.open('https://remaxjs.org/');
-            }}
-          >
-            {logo}
-            {title}
-          </div>
-        )}
         {...defaultProps}
         location={{
           pathname: '/welcome',
