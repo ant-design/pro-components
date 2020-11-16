5415b095332e5a95bfa573d70b5bf8d2026582dd
大猫 <rdmclin2@163.com>
2020-11-16 17:34:59 +0800

    feat(layout): fix ghost mode content padding

---
+++ packages/layout/src/demos/ghost.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import ProLayout, { PageContainer } from '@ant-design/pro-layout';
+import ProCard from '@ant-design/pro-card';
+import { Descriptions } from 'antd';
+import defaultProps from './_defaultProps';
+
+const content = (
+  <Descriptions size="small" column={2}>
+    <Descriptions.Item label="创建人">张三</Descriptions.Item>
+    <Descriptions.Item label="联系方式">
+      <a>421421</a>
+    </Descriptions.Item>
+    <Descriptions.Item label="创建时间">2017-01-10</Descriptions.Item>
+    <Descriptions.Item label="更新时间">2017-10-10</Descriptions.Item>
+    <Descriptions.Item label="备注">中国浙江省杭州市西湖区古翠路</Descriptions.Item>
+  </Descriptions>
+);
+
+export default () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+      }}
+    >
+      <ProLayout
+        menuItemRender={(item, dom) => <div>pre {dom}</div>}
+        subMenuItemRender={(_, dom) => <div>pre {dom}</div>}
+        title="Remax"
+        logo="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*1NHAQYduQiQAAAAAAAAAAABkARQnAQ"
+        menuHeaderRender={(logo, title) => (
+          <div
+            id="customize_menu_header"
+            onClick={() => {
+              window.open('https://remaxjs.org/');
+            }}
+          >
+            {logo}
+            {title}
+          </div>
+        )}
+        {...defaultProps}
+        location={{
+          pathname: '/welcome',
+        }}
+      >
+        <PageContainer ghost content={content}>
+          <ProCard direction="column" ghost gutter={[0, 16]}>
+            <ProCard style={{ height: 200 }} />
+            <ProCard gutter={16} ghost style={{ height: 200 }}>
+              <ProCard colSpan={16} />
+              <ProCard colSpan={8} />
+            </ProCard>
+          </ProCard>
+        </PageContainer>
+      </ProLayout>
+    </div>
+  );
+};
