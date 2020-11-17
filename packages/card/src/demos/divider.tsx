89c813d9d344eaa8ea4963839847ec5c0c2ef017
大猫 <rdmclin2@gmail.com>
2020-11-17 09:51:32 +0800

    feat(card): pro-card support Group and Divider (#992)
    * feat: add divider and group
    
    * chore(card): update card snapshot
    
    * chore(card): move sub components to dir
    
    * chore(card): sub components

---
+++ packages/card/src/demos/divider.tsx
@@ -0,0 +1,31 @@
+/**
+ * uuid: ff1e143f
+ * title: 分组指标
+ */
+
+import React from 'react';
+import { Statistic } from 'antd';
+import ProCard from '@ant-design/pro-card';
+
+const { Divider } = ProCard;
+
+export default () => {
+  return (
+    <ProCard.Group title="核心指标">
+      <ProCard>
+        <Statistic title="今日UV" value={79.0} precision={2} />
+      </ProCard>
+      <Divider />
+      <ProCard>
+        <Statistic title="冻结金额" value={112893.0} precision={2} />
+      </ProCard>
+      <Divider />
+      <ProCard>
+        <Statistic title="信息完整度" value={93} suffix="/ 100" />
+      </ProCard>
+      <ProCard>
+        <Statistic title="冻结金额" value={112893.0} />
+      </ProCard>
+    </ProCard.Group>
+  );
+};
