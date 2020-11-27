ea009fc4983a27fad19a49aea5285b20da37af63
Tom Xu <ycxzhkx@gmail.com>
2020-11-27 08:32:09 +0800

    feat: add test

---
+++ tests/table/polling.test.tsx
@@ -0,0 +1,37 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import ProTable from '@ant-design/pro-table';
+import { act } from 'react-dom/test-utils';
+
+import { waitForComponentToPaint } from '../util';
+
+describe('Table polling', () => {
+  it('function', async () => {
+    const onRequest = jest.fn();
+    const html = mount(
+      <ProTable
+        size="small"
+        polling={() => 1000}
+        request={async () => {
+          onRequest();
+          return { data: [] };
+        }}
+        columns={[
+          {
+            title: '状态',
+            dataIndex: 'status',
+            hideInForm: true,
+            valueEnum: {},
+            fieldProps: {
+              open: true,
+            },
+          },
+        ]}
+        rowKey="key"
+      />,
+    );
+    await waitForComponentToPaint(html, 1500);
+
+    expect(onRequest).toHaveBeenCalledTimes(2);
+  });
+});
