8cfb3b6866b5ac594db0e4b02193672846c1c514
Dedy Simamora <DS37676X@cimbniaga.co.id>
2020-12-04 11:50:58 +0700

    localization: added idr_ID

---
+++ packages/provider/src/locale/id_ID.tsx
@@ -0,0 +1,56 @@
+export default {
+  moneySymbol: 'RP',
+  form: {
+    lightFilter: {
+      more: 'Lebih',
+      clear: 'Hapus',
+      confirm: 'Konfirmasi',
+      itemUnit: 'Unit',
+    },
+  },
+  tableForm: {
+    search: 'Cari',
+    reset: 'Atur ulang',
+    submit: 'Kirim',
+    collapsed: 'Lebih sedikit',
+    expand: 'Lebih banyak',
+    inputPlaceholder: 'Masukkan pencarian',
+    selectPlaceholder: 'Pilih',
+  },
+  alert: {
+    clear: 'Hapus',
+    selected: 'Dipilih',
+    item: 'Butir',
+  },
+  pagination: {
+    total: {
+      range: ' ',
+      total: 'Dari',
+      item: 'Butir',
+    },
+  },
+  tableToolBar: {
+    leftPin: 'Pin kiri',
+    rightPin: 'Pin kanan',
+    noPin: 'Tidak ada pin',
+    leftFixedTitle: 'Rata kiri',
+    rightFixedTitle: 'Rata kanan',
+    noFixedTitle: 'Tidak tetap',
+    reset: 'Atur ulang',
+    columnDisplay: 'Tampilan kolom',
+    columnSetting: 'Pengaturan',
+    fullScreen: 'Layar penuh',
+    exitFullScreen: 'Keluar layar penuh',
+    reload: 'Atur ulang',
+    density: 'Kerapatan',
+    densityDefault: 'Standar',
+    densityLarger: 'Lebih besar',
+    densityMiddle: 'Sedang',
+    densitySmall: 'Rapat',
+  },
+  stepsForm: {
+    next: 'Selanjutnya',
+    prev: 'Sebelumnya',
+    submit: 'Selesai',
+  },
+};
