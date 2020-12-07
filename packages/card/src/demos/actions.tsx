9a2562bfa5c9ccb6eeba3879f061e76803cc0492
大猫 <rdmclin2@163.com>
2020-12-07 17:25:17 +0800

    feat(card): add card actions feature

---
+++ packages/card/src/demos/actions.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ProCard from '@ant-design/pro-card';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+export default () => {
+  return (
+    <>
+      <ProCard
+        title="Actions 操作项"
+        style={{ width: 300 }}
+        actions={[
+          <SettingOutlined key="setting" />,
+          <EditOutlined key="edit" />,
+          <EllipsisOutlined key="ellipsis" />,
+        ]}
+      >
+        <div>Card content</div>
+        <div>Card content</div>
+        <div>Card content</div>
+      </ProCard>
+    </>
+  );
+};
