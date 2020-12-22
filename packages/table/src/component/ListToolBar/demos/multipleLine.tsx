95d2d395635adde8b4739747b31bf23fca2f78f2
大猫 <rdmclin2@163.com>
2020-12-22 15:49:15 +0800

    feat: embed ListToolBar

--- packages/table/src/component/ListToolBar/demos/multipleLine.tsx
+++ packages/table/src/component/ListToolBar/demos/multipleLine.tsx
@@ -1,5 +1,10 @@
+/**
+ * title: 双行布局
+ * desc: 双行的情况下会有双行的布局形式。
+ */
 import React from 'react';
-import { EllipsisOutlined } from '@ant-design/icons';
+import { Button, Dropdown, Menu } from 'antd';
+import { EllipsisOutlined, DownOutlined } from '@ant-design/icons';
 import { LightFilter, ProFormDatePicker } from '@ant-design/pro-form';
 import type { ProColumns } from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
@@ -75,26 +80,49 @@ export default () => {
           success: true,
         });
       }}
+      headerTitle="两行的情况"
       toolbar={{
-        title: '标签',
         multipleLine: true,
+        search: {
+          onSearch: (value) => {
+            alert(value);
+          },
+        },
         filter: (
           <LightFilter>
             <ProFormDatePicker name="startdate" label="响应日期" />
           </LightFilter>
         ),
-        tabs: {
-          items: [
-            {
-              key: 'tab1',
-              tab: '标签一',
-            },
-            {
-              key: 'tab2',
-              tab: '标签二',
-            },
-          ],
-        },
+        actions: [
+          <Dropdown
+            key="overlay"
+            overlay={
+              <Menu onClick={() => alert('menu click')}>
+                <Menu.Item key="1">菜单</Menu.Item>
+                <Menu.Item key="2">列表</Menu.Item>
+                <Menu.Item key="3">表单</Menu.Item>
+              </Menu>
+            }
+          >
+            <Button>
+              移动自
+              <DownOutlined
+                style={{
+                  marginLeft: 8,
+                }}
+              />
+            </Button>
+          </Dropdown>,
+          <Button
+            key="add"
+            type="primary"
+            onClick={() => {
+              alert('add');
+            }}
+          >
+            添加
+          </Button>,
+        ],
       }}
       rowKey="key"
       search={false}
