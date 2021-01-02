454ac66792918a90bf01c1a924943d9d8c75da08
chenshuai2144 <qixian.cs@outlook.com>
2021-01-02 23:11:37 +0800

    feat(from): support ProFormDependency

---
+++ packages/form/src/components/Dependency/index.tsx
@@ -0,0 +1,39 @@
+import { Form } from 'antd';
+import type { NamePath } from 'antd/lib/form/interface';
+import type { FormInstance, FormItemProps } from 'antd/lib/form';
+import get from 'rc-util/lib/utils/get';
+
+declare type RenderChildren<Values = any> = (
+  values: Record<string, any>,
+  form: FormInstance<Values>,
+) => React.ReactNode;
+
+export type ProFormDependencyProps = Omit<
+  FormItemProps<any>,
+  'name' | 'noStyle' | 'children' | 'label'
+> & {
+  name: NamePath[];
+  children: RenderChildren;
+};
+
+const ProFormDependency: React.FC<ProFormDependencyProps> = ({ name, children, ...rest }) => {
+  return (
+    <Form.Item
+      {...rest}
+      noStyle
+      shouldUpdate={(prevValues, nextValues) => {
+        return name.some((nameItem) => {
+          const arrayName = Array.isArray(nameItem) ? nameItem : [nameItem];
+          return get(prevValues, arrayName) !== get(nextValues, arrayName);
+        });
+      }}
+    >
+      {(form) => {
+        const values = form.getFieldsValue(name);
+        return children?.(values, form as FormInstance<any>);
+      }}
+    </Form.Item>
+  );
+};
+
+export default ProFormDependency;
