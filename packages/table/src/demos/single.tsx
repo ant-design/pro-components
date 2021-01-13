f933c05395f1db415c6c4ad8f7da426fda20903d
chenshuai2144 <qixian.cs@outlook.com>
2021-01-13 18:19:31 +0800

    fix ci

--- packages/table/src/demos/single.tsx
+++ packages/table/src/demos/single.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { PlusOutlined, EllipsisOutlined } from '@ant-design/icons';
 import { Button, Tag, Space, Menu, Dropdown } from 'antd';
 import type { ProColumns, ActionType } from '@ant-design/pro-table';
@@ -126,10 +126,7 @@ const menu = (
 
 export default () => {
   const actionRef = useRef<ActionType>();
-  const formRef = useRef<ActionType>();
-  useEffect(() => {
-    console.log(formRef.current);
-  }, []);
+
   return (
     <ProTable<GithubIssueItem>
       columns={columns}
@@ -144,7 +141,6 @@ export default () => {
       editable={{
         type: 'multiple',
       }}
-      formRef={formRef}
       rowKey="id"
       search={{
         labelWidth: 'auto',
