97f5c3aba4563b26522585aa1237918622811bf4
chenshuai2144 <qixian.cs@outlook.com>
2021-01-17 12:08:55 +0800

    support valueType

--- tests/form/base.test.tsx
+++ tests/form/base.test.tsx
@@ -18,6 +18,16 @@ describe('ProForm', () => {
     expect(wrapper.render()).toMatchSnapshot();
   });
 
+  it('📦 ProForm support sync form url', async () => {
+    const wrapper = mount(
+      <ProForm syncToUrl>
+        <ProFormText name="navTheme" />
+      </ProForm>,
+    );
+    await waitForComponentToPaint(wrapper);
+    expect(wrapper.render()).toMatchSnapshot();
+  });
+
   it('📦 onFinish should simulate button loading', async () => {
     const fn = jest.fn();
     const wrapper = mount(
