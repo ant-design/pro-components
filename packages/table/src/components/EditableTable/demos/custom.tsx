277e018e5c96a93d5cbf67c4c2cadc728ef9309b
chenshuai2144 <qixian.cs@outlook.com>
2021-01-21 11:52:32 +0800

    更好的demo

--- packages/table/src/components/EditableTable/demos/custom.tsx
+++ packages/table/src/components/EditableTable/demos/custom.tsx
@@ -204,29 +204,7 @@ export default () => {
             await waitTime(2000);
           },
           onChange: setEditableRowKeys,
-          actionRender: (row, config) => [
-            <a
-              key="save"
-              onClick={async () => {
-                const values = (await config?.form?.validateFields()) as DataSourceType;
-                const hide = message.loading('保存中。。。');
-                await config?.onSave?.(config.recordKey, { ...row, ...values });
-                hide();
-              }}
-            >
-              保存
-            </a>,
-            <a
-              key="cancel"
-              onClick={async () => {
-                const values = (await config?.form?.validateFields()) as DataSourceType;
-                config?.cancelEditable(config.recordKey);
-                await config?.onCancel?.(config.recordKey, { ...row, ...values });
-              }}
-            >
-              取消
-            </a>,
-          ],
+          actionRender: (row, config, dom) => [dom.save, dom.cancel],
         }}
       />
       <ProCard title="表格数据" headerBordered collapsible defaultCollapsed>
