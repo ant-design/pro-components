041d517918d95c9ae630e6f17a104c7cd7aaa8d7
chenshuai2144 <qixian.cs@outlook.com>
2021-01-26 09:45:41 +0800

    fix test

--- packages/utils/src/useEditableArray/index.tsx
+++ packages/utils/src/useEditableArray/index.tsx
@@ -1,8 +1,8 @@
 ﻿import React, { useCallback, useMemo, useRef, useState } from 'react';
-import type { GetRowKey } from 'antd/es/table/interface';
+import type { GetRowKey } from 'antd/lib/table/interface';
 import useMergedState from 'rc-util/es/hooks/useMergedState';
 import type { FormInstance } from 'antd';
-import useLazyKVMap from 'antd/es/table/hooks/useLazyKVMap';
+import useLazyKVMap from 'antd/lib/table/hooks/useLazyKVMap';
 import { LoadingOutlined } from '@ant-design/icons';
 import { useIntl } from '@ant-design/pro-provider';
 import { message, Popconfirm } from 'antd';
