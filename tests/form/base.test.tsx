9d31f0807b501f249849a724e586673a82a2a88b
chenshuai2144 <qixian.cs@outlook.com>
2021-02-19 22:38:15 +0800

    add test

--- tests/form/base.test.tsx
+++ tests/form/base.test.tsx
@@ -881,6 +881,79 @@ describe('ProForm', () => {
     expect(wrapper.find('.ant-select-item').length).toBe(4);
   });
 
+  it('📦 SearchSelect support searchOnFocus', async () => {
+    const onSearch = jest.fn();
+    const wrapper = mount(
+      <ProForm>
+        <ProFormSelect.SearchSelect
+          name="userQuery"
+          label="查询选择器"
+          fieldProps={{
+            resetAfterSelect: true,
+            onSearch: (e) => onSearch(e),
+          }}
+          valueEnum={{
+            all: { text: '全部', status: 'Default' },
+            open: {
+              text: '未解决',
+              status: 'Error',
+            },
+            closed: {
+              text: '已解决',
+              status: 'Success',
+            },
+            processing: {
+              text: '解决中',
+              status: 'Processing',
+            },
+          }}
+        />
+      </ProForm>,
+    );
+    await waitForComponentToPaint(wrapper);
+
+    act(() => {
+      wrapper.find('.ant-select-selection-search-input').simulate('change', {
+        target: {
+          value: '全',
+        },
+      });
+    });
+    await waitForComponentToPaint(wrapper);
+
+    expect(onSearch).toBeCalledWith('全');
+
+    act(() => {
+      wrapper.find('.ant-select-selector').simulate('mousedown');
+      wrapper.update();
+    });
+
+    expect(wrapper.find('.ant-select-item').length).toBe(1);
+    expect(wrapper.find('.ant-select-item-option-content div span').text()).toBe('全');
+
+    await waitForComponentToPaint(wrapper);
+
+    act(() => {
+      wrapper.find('.ant-select-selector').simulate('mousedown');
+      wrapper.update();
+    });
+
+    // 选中第一个
+    act(() => {
+      wrapper.find('.ant-select-item').at(0).simulate('click');
+    });
+
+    await waitForComponentToPaint(wrapper);
+
+    act(() => {
+      wrapper.find('.ant-select-selector').simulate('mousedown');
+      wrapper.update();
+    });
+
+    await waitForComponentToPaint(wrapper);
+    expect(wrapper.find('.ant-select-item').length).toBe(4);
+  });
+
   it('📦 SearchSelect support multiple', async () => {
     const onSearch = jest.fn();
     const onFinish = jest.fn();
