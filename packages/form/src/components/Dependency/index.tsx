bf3d45171ac9be0c7672c2992bef2d4de7655037
chenshuai2144 <qixian.cs@outlook.com>
2021-02-21 22:39:42 +0800

    docs: add better demos

--- packages/form/src/components/Dependency/index.tsx
+++ packages/form/src/components/Dependency/index.tsx
@@ -2,6 +2,9 @@ import { Form } from 'antd';
 import type { NamePath } from 'antd/lib/form/interface';
 import type { FormInstance, FormItemProps } from 'antd';
 import get from 'rc-util/lib/utils/get';
+import { useCallback, useContext, useMemo } from 'react';
+import set from 'rc-util/lib/utils/set';
+import { FormListContext } from '../List';
 
 declare type RenderChildren<Values = any> = (
   values: Record<string, any>,
@@ -17,20 +20,58 @@ export type ProFormDependencyProps = Omit<
 };
 
 const ProFormDependency: React.FC<ProFormDependencyProps> = ({ name, children, ...rest }) => {
+  // ProFromList 的 filed，里面有name和key
+  const formListField = useContext(FormListContext);
+  /**
+   * 处理放到 Form.List 的情况
+   *
+   * @param itemName
+   */
+  const getNamePath = useCallback(
+    (itemName: NamePath) => {
+      if (formListField.name === undefined) {
+        return [itemName].flat(1) as string[];
+      }
+      return [formListField.listName, formListField.name, itemName].flat(1) as string[];
+    },
+    [formListField.listName, formListField.name],
+  );
+
+  const names = useMemo(() => {
+    if (formListField.name === undefined) {
+      return name;
+    }
+    return name.map((itemName: NamePath) => {
+      return [formListField.listName, formListField.name, itemName].flat(1) as string[];
+    });
+  }, [formListField.listName, formListField.name, name]);
+
   return (
     <Form.Item
       {...rest}
       noStyle
       shouldUpdate={(prevValues, nextValues) => {
-        return name.some((nameItem) => {
+        return names.some((nameItem) => {
           const arrayName = Array.isArray(nameItem) ? nameItem : [nameItem];
           return get(prevValues, arrayName) !== get(nextValues, arrayName);
         });
       }}
     >
       {(form) => {
-        const values = form.getFieldsValue(name);
-        return children?.(values, form as FormInstance<any>);
+        const values = form.getFieldsValue(names);
+
+        const nameValues = name
+          .map((itemName) => {
+            const namePath = getNamePath(itemName);
+            return set({}, [itemName].flat(1) as string[], get(values, namePath));
+          })
+          .reduce((pre, next) => {
+            return {
+              ...pre,
+              ...next,
+            };
+          }, {});
+        return children?.(nameValues, form as FormInstance<any>);
       }}
     </Form.Item>
   );
