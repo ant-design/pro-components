a245e3cb669a59cab82791e20fcca8423d989ea8
大猫 <rdmclin2@gmail.com>
2021-03-19 18:39:47 +0800

    docs: PageContainer demo background (#2242)
    * chore: change pagecontainer demo background
    
    * chore: update snapshot

--- packages/layout/src/components/PageContainer/demos/hideBreadMenu.tsx
+++ packages/layout/src/components/PageContainer/demos/hideBreadMenu.tsx
@@ -4,28 +4,35 @@ import { PageContainer } from '@ant-design/pro-layout';
 import ProCard from '@ant-design/pro-card';
 
 export default () => (
-  <PageContainer
-    ghost
-    header={{
-      title: '页面标题',
+  <div
+    style={{
+      background: '#F5F7FA',
     }}
-    content={
-      <Descriptions column={2} style={{ marginBottom: -16 }}>
-        <Descriptions.Item label="创建人">曲丽丽</Descriptions.Item>
-        <Descriptions.Item label="关联表单">
-          <a>421421</a>
-        </Descriptions.Item>
-        <Descriptions.Item label="创建时间">2017-01-10</Descriptions.Item>
-        <Descriptions.Item label="单据备注">浙江省杭州市西湖区工专路</Descriptions.Item>
-      </Descriptions>
-    }
   >
-    <ProCard direction="column" ghost gutter={[0, 16]}>
-      <ProCard style={{ height: 200 }} />
-      <ProCard gutter={16} ghost style={{ height: 200 }}>
-        <ProCard colSpan={16} />
-        <ProCard colSpan={8} />
+    <PageContainer
+      ghost
+      header={{
+        title: '页面标题',
+        breadcrumb: {},
+      }}
+      content={
+        <Descriptions column={2} style={{ marginBottom: -16 }}>
+          <Descriptions.Item label="创建人">曲丽丽</Descriptions.Item>
+          <Descriptions.Item label="关联表单">
+            <a>421421</a>
+          </Descriptions.Item>
+          <Descriptions.Item label="创建时间">2017-01-10</Descriptions.Item>
+          <Descriptions.Item label="单据备注">浙江省杭州市西湖区工专路</Descriptions.Item>
+        </Descriptions>
+      }
+    >
+      <ProCard direction="column" ghost gutter={[0, 16]}>
+        <ProCard style={{ height: 200 }} />
+        <ProCard gutter={16} ghost style={{ height: 200 }}>
+          <ProCard colSpan={16} />
+          <ProCard colSpan={8} />
+        </ProCard>
       </ProCard>
-    </ProCard>
-  </PageContainer>
+    </PageContainer>
+  </div>
 );
