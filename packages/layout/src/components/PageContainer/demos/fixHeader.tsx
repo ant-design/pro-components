5c22213c92fdb0b1d8b36ea48684e458b64630f0
大猫 <rdmclin2@163.com>
2021-03-19 12:12:15 +0800

    chore: change pagecontainer demo background

--- packages/layout/src/components/PageContainer/demos/fixHeader.tsx
+++ packages/layout/src/components/PageContainer/demos/fixHeader.tsx
@@ -3,49 +3,55 @@ import { PageContainer } from '@ant-design/pro-layout';
 import ProCard from '@ant-design/pro-card';
 
 export default () => (
-  <PageContainer
-    fixedHeader
-    header={{
-      title: '页面标题',
-      breadcrumb: {
-        routes: [
-          {
-            path: '',
-            breadcrumbName: '一级页面',
-          },
-          {
-            path: '',
-            breadcrumbName: '二级页面',
-          },
-          {
-            path: '',
-            breadcrumbName: '当前页面',
-          },
-        ],
-      },
+  <div
+    style={{
+      background: '#F5F7FA',
     }}
-    tabList={[
-      {
-        tab: '已选择',
-        key: '1',
-      },
-      {
-        tab: '可点击',
-        key: '2',
-      },
-      {
-        tab: '禁用',
-        key: '3',
-        disabled: true,
-      },
-    ]}
   >
-    <ProCard direction="column" ghost gutter={[0, 16]}>
-      <ProCard style={{ height: 200 }} />
-      <ProCard gutter={16} ghost style={{ height: 200 }}>
-        <ProCard colSpan={16} />
-        <ProCard colSpan={8} />
+    <PageContainer
+      fixedHeader
+      header={{
+        title: '页面标题',
+        breadcrumb: {
+          routes: [
+            {
+              path: '',
+              breadcrumbName: '一级页面',
+            },
+            {
+              path: '',
+              breadcrumbName: '二级页面',
+            },
+            {
+              path: '',
+              breadcrumbName: '当前页面',
+            },
+          ],
+        },
+      }}
+      tabList={[
+        {
+          tab: '已选择',
+          key: '1',
+        },
+        {
+          tab: '可点击',
+          key: '2',
+        },
+        {
+          tab: '禁用',
+          key: '3',
+          disabled: true,
+        },
+      ]}
+    >
+      <ProCard direction="column" ghost gutter={[0, 16]}>
+        <ProCard style={{ height: 200 }} />
+        <ProCard gutter={16} ghost style={{ height: 200 }}>
+          <ProCard colSpan={16} />
+          <ProCard colSpan={8} />
+        </ProCard>
       </ProCard>
-    </ProCard>
-  </PageContainer>
+    </PageContainer>
+  </div>
 );
