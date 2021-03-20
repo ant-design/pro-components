add3fc94933e5ed3e8da40ff50127a4f2e3b3b56
mouth <gloria_mouth@126.com>
2021-03-20 19:00:06 +0800

    docs: add modalForm reset demos. (#2252)
    * docs: add modalForm reset demos.
    
    * update snapshot
    
    Co-authored-by: chenshuai2144 <qixian.cs@outlook.com>

--- packages/form/src/components/ModalForm/demos/modal-form-reset.tsx
+++ packages/form/src/components/ModalForm/demos/modal-form-reset.tsx
@@ -1,5 +1,6 @@
-﻿import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, message, Space } from 'antd';
+import type { FormInstance } from 'antd';
 import { ModalForm, ProFormText } from '@ant-design/pro-form';
 
 const waitTime = (time: number = 100) => {
@@ -11,80 +12,71 @@ const waitTime = (time: number = 100) => {
 };
 
 export default () => {
+  const formRef = useRef<FormInstance>();
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+
   return (
     <Space>
       <ModalForm
         title="新建表单"
-        trigger={<Button type="primary">自己定义 footer 的按钮</Button>}
+        formRef={formRef}
+        visible={modalVisible}
+        trigger={
+          <Button
+            type="primary"
+            onClick={() => {
+              setModalVisible(true);
+            }}
+          >
+            通过 formRef 重置
+          </Button>
+        }
+        onVisibleChange={setModalVisible}
+        submitter={{
+          searchConfig: {
+            resetText: '重置',
+          },
+          resetButtonProps: {
+            onClick: () => {
+              formRef.current?.resetFields();
+              //   setModalVisible(false);
+            },
+          },
+        }}
+        onFinish={async (values) => {
+          await waitTime(2000);
+          console.log(values);
+          message.success('提交成功');
+          return true;
+        }}
+      >
+        <ProFormText
+          width="md"
+          name="name"
+          label="签约客户名称"
+          tooltip="最长为 24 位"
+          placeholder="请输入名称"
+        />
+
+        <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
+      </ModalForm>
+      <ModalForm
+        title="新建表单"
+        formRef={formRef}
+        trigger={<Button type="primary">通过自定义 footer 按钮重置</Button>}
         submitter={{
           render: (props, defaultDoms) => {
             return [
               ...defaultDoms,
               <Button
-                key="ok"
+                key="extra-reset"
                 onClick={() => {
-                  props.submit();
+                  props.reset();
                 }}
               >
-                ok
+                重置
               </Button>,
             ];
-          },
-        }}
-        onFinish={async (values) => {
-          await waitTime(2000);
-          console.log(values);
-          message.success('提交成功');
-          return true;
-        }}
-      >
-        <ProFormText
-          width="md"
-          name="name"
-          label="签约客户名称"
-          tooltip="最长为 24 位"
-          placeholder="请输入名称"
-        />
-
-        <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
-      </ModalForm>
-      <ModalForm
-        title="新建表单"
-        trigger={<Button type="primary">自定义文字</Button>}
-        submitter={{
-          searchConfig: {
-            submitText: '确认',
-            resetText: '取消',
-          },
-        }}
-        onFinish={async (values) => {
-          await waitTime(2000);
-          console.log(values);
-          message.success('提交成功');
-          return true;
-        }}
-      >
-        <ProFormText
-          width="md"
-          name="name"
-          label="签约客户名称"
-          tooltip="最长为 24 位"
-          placeholder="请输入名称"
-        />
-
-        <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
-      </ModalForm>
-      <ModalForm
-        title="新建表单"
-        trigger={<Button type="primary">隐藏或修改按钮样式</Button>}
-        submitter={{
-          resetButtonProps: {
-            type: 'dashed',
-          },
-          submitButtonProps: {
-            style: {
-              display: 'none',
-            },
           },
         }}
         onFinish={async (values) => {
