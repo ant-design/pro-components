c6ba669ac7c14332646789c694ed7e6c50f8a85d
drizzlesconsin <69514654+drizzlesconsin@users.noreply.github.com>
2021-03-23 19:19:16 +0800

    fix: syntax error

--- packages/table/src/components/Form/FormRender.tsx
+++ packages/table/src/components/Form/FormRender.tsx
@@ -416,8 +416,8 @@ const FormRender = <T, U = any>({
           [formClassName]: isForm,
           [getPrefixCls(`pro-table-search-${competentName}`)]: true,
           [`${getPrefixCls('card')}-bordered`]: !!bordered,
-        },
-        [searchConfig.className]: searchConfig !== false && searchConfig?.className ,
+          [searchConfig.className]: searchConfig !== false && searchConfig?.className,
+        }        
       )}
     >
       <Competent
