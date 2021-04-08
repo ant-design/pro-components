c39715d5dc57f11701764429034f4f1b666a1a42
chenshuai2144 <qixian.cs@outlook.com>
2021-04-08 14:42:48 +0800

    fix test

--- packages/table/src/utils/cellRenderToFromItem.tsx
+++ packages/table/src/utils/cellRenderToFromItem.tsx
@@ -142,7 +142,7 @@ function cellRenderToFromItem<T>(config: {
               name={name}
               {...formItemProps}
               messageVariables={messageVariables}
-              initialValue={text ?? formItemProps?.initialValue}}
+              initialValue={text ?? formItemProps?.initialValue}
             >
               {inputDom}
             </InlineErrorFormItem>
@@ -163,7 +163,7 @@ function cellRenderToFromItem<T>(config: {
                 name={name}
                 {...formItemProps}
                 messageVariables={messageVariables}
-                initialValue={text ?? formItemProps?.initialValue}}
+                initialValue={text ?? formItemProps?.initialValue}
               >
                 {inputDom}
               </InlineErrorFormItem>
@@ -183,7 +183,7 @@ function cellRenderToFromItem<T>(config: {
               columnProps?.key || columnProps?.dataIndex || config.index,
             )}
             {...formItemProps}
-            initialValue={text ?? formItemProps?.initialValue}}
+            initialValue={text ?? formItemProps?.initialValue}
             messageVariables={messageVariables}
           >
             {renderDom}
