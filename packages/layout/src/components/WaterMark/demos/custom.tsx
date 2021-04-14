df8b5cae4311bfb6f910880385c0e4f6195d03ab
chenshuai2144 <qixian.cs@outlook.com>
2021-04-14 11:27:00 +0800

    fix test

--- packages/layout/src/components/WaterMark/demos/custom.tsx
+++ packages/layout/src/components/WaterMark/demos/custom.tsx
@@ -68,7 +68,7 @@ export default () => {
                     src="https://gw.alipayobjects.com/zos/bmw-prod/d283f09a-64d6-4d59-bfc7-37b49ea0da2b.svg"
                     alt="示例图片"
                     width={600}
-                    style={{ zIndex: 10, position: 'relative' }}
+                    style={{ zIndex: 10, maxWidth: '100%', position: 'relative' }}
                   />
                 </WaterMark>
               );
