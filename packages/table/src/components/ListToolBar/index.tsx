de1cd0c04cbde0e5199e640457a31b94f47b00b8
wenhaoming <435203093@qq.com>
2021-04-28 23:15:00 +0800

    fix: 修复 ProTable 轻量筛选，小屏幕下变形

--- packages/table/src/components/ListToolBar/index.tsx
+++ packages/table/src/components/ListToolBar/index.tsx
@@ -183,7 +183,7 @@ const ListToolBar: React.FC<ListToolBarProps> = ({
           {menu && <HeaderMenu {...menu} prefixCls={prefixCls} />}
           {!hasTitle && searchNode && <div className={`${prefixCls}-search`}>{searchNode}</div>}
         </Space>
-        <Space className={`${prefixCls}-right`} size={16}>
+        <Space className={`${prefixCls}-right`} size={16} wrap>
           {hasTitle && searchNode ? (
             <div className={`${prefixCls}-search`}>{searchNode}</div>
           ) : null}
