e2e0232adda8d94b5cf0d40497dee01cebf2af73
纪颖志 <jiyingzhi0808@gmail.com>
2021-05-10 15:55:49 +0800

    feat(layout): support for all parameters of the Spin component (#2704)
    * feat: support for all parameters of the Spin component
    
    * chore: add test
    
    Co-authored-by: 纪颖志 <“jiyingzhi@black-unique.com”>

--- packages/layout/src/components/PageLoading/index.tsx
+++ packages/layout/src/components/PageLoading/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Spin } from 'antd';
+import type { SpinProps } from 'antd';
 
-const PageLoading: React.FC<{
-  tip?: string;
-}> = ({ tip }) => (
+const PageLoading: React.FC<SpinProps> = (props) => (
   <div style={{ paddingTop: 100, textAlign: 'center' }}>
-    <Spin size="large" tip={tip} />
+    <Spin size="large" {...props} />
   </div>
 );
 
