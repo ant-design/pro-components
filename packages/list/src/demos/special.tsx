e4835a2ce35e65a9cddab8aedc47aaa783bb6e81
chenshuai2144 <qixian.cs@outlook.com>
2021-06-23 09:14:31 +0800

    fix test

--- packages/list/src/demos/special.tsx
+++ packages/list/src/demos/special.tsx
@@ -66,7 +66,6 @@ export default () => {
         avatar: string;
         children: JSX.Element;
       }>
-        itemTitleRender={() => `qixian`}
         metas={{
           title: {},
           subTitle: {},
