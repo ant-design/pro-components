08a6928130d9f74f6d5ec5f8d78a4997da0caeb4
Derrick <547791743@qq.com>
2021-07-13 16:27:15 +0800

    ✨ feat(form): feat new valueType -> divider (#3215)
    * ✨ feat(form): feat new valueType -> divider
    
    * fix: add readonly
    
    * fix: delete readonly

--- packages/form/src/components/SchemaForm/index.tsx
+++ packages/form/src/components/SchemaForm/index.tsx
@@ -1,5 +1,6 @@
 ﻿import React, { useCallback, useMemo, useRef, useState } from 'react';
 import type { FormInstance, FormProps } from 'antd';
+import { Divider } from 'antd';
 import type {
   DrawerFormProps,
   QueryFilterProps,
@@ -60,7 +61,7 @@ export type ProFormPropsType<T> = Omit<DrawerFormProps<T>, 'onFinish'> &
     layoutType?: ProFormLayoutType;
   };
 
-export type FormFieldType = 'group' | 'formList' | 'formSet';
+export type FormFieldType = 'group' | 'formList' | 'formSet' | 'divider';
 
 export type ProFormColumnsType<T = any, ValueType = 'text'> = ProSchema<
   T,
@@ -229,6 +230,12 @@ function BetaSchemaForm<T, ValueType = 'text'>(props: FormSchema<T, ValueType>) 
               </ProFormFieldSet>
             );
           }
+
+          /** 分割线 */
+          if (item.valueType === 'divider') {
+            return <Divider {...item.fieldProps} />;
+          }
+
           /** 公用的 类型 props */
           const formFieldProps: ProFormFieldProps = {
             key,
