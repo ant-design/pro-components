bdc331ccd5692a725ea2427be87403a5f3aee925
gargantuax <gargantuax@qq.com>
2021-08-18 17:32:15 +0800

    fix(layout): 添加测试，文档，demo

--- tests/layout/index.test.tsx
+++ tests/layout/index.test.tsx
@@ -1134,4 +1134,193 @@ describe('BasicLayout', () => {
     await waitForComponentToPaint(html, 100);
     expect(fn).toBeCalledTimes(3);
   });
+
+  it('🥩 BasicLayout support menu.defaultOpenAll', async () => {
+    const Demo = () => {
+      const [pathname, setPathname] = useState('/admin/sub-page1');
+      return (
+        <BasicLayout
+          menu={{
+            defaultOpenAll: true,
+          }}
+          location={{ pathname }}
+          menuItemRender={(item, dom) => (
+            <a
+              onClick={() => {
+                item.onClick();
+                setPathname(item.path || '/welcome');
+              }}
+            >
+              {dom}
+            </a>
+          )}
+          menuDataRender={() => [
+            {
+              path: '/home',
+              name: '首页',
+              locale: 'menu.home',
+              children: [
+                {
+                  path: '/home/overview',
+                  name: '概述',
+                  hideInMenu: true,
+                  locale: 'menu.home.overview',
+                },
+                {
+                  path: '/home/search',
+                  name: '搜索',
+                  hideInMenu: true,
+                  locale: 'menu.home.search',
+                },
+              ],
+            },
+            {
+              path: '/data_hui',
+              name: '汇总数据',
+              locale: 'menu.data_hui',
+              children: [
+                {
+                  collapsed: true,
+                  menuName: '域买家维度交易',
+                  name: '域买家维度交易',
+                  children: [
+                    {
+                      id: 2,
+                      name: '月表',
+                      path: '/data_hui2',
+                    },
+                    {
+                      name: '日表',
+                      path: '/data_hui3?tableName=adm_rk_cr_tb_trv_byr_ds&tableSchema=alifin_odps_birisk',
+                    },
+                  ],
+                },
+                {
+                  name: '维度交易',
+                  path: '/',
+                  children: [
+                    {
+                      name: '月表',
+                      path: '/data_hui4',
+                    },
+                    {
+                      name: '日表',
+                      key: 'tableName=adm_rk_cr_tb_trv_byr_ds&tableSchema=alifin_odps_birisk',
+                      path: '/data_hui5',
+                    },
+                  ],
+                },
+              ],
+            },
+          ]}
+        />
+      );
+    };
+    const html = mount(<Demo />);
+    await waitForComponentToPaint(html);
+
+    expect(html.find('li.ant-menu-submenu').length).toBe(3);
+    expect(html.find('li.ant-menu-submenu-open').length).toBe(3);
+  });
+
+  it('🥩 BasicLayout support menu.ignoreFlatMenu', async () => {
+    const Demo = () => {
+      const [pathname, setPathname] = useState('/admin/sub-page1');
+      return (
+        <BasicLayout
+          menu={{
+            defaultOpenAll: true,
+            ignoreFlatMenu: true,
+          }}
+          location={{ pathname }}
+          menuItemRender={(item, dom) => (
+            <a
+              onClick={() => {
+                item.onClick();
+                setPathname(item.path || '/welcome');
+              }}
+            >
+              {dom}
+            </a>
+          )}
+          menuDataRender={() => [
+            {
+              path: '/home',
+              name: '首页',
+              locale: 'menu.home',
+              children: [
+                {
+                  path: '/home/overview',
+                  name: '概述',
+                  hideInMenu: true,
+                  locale: 'menu.home.overview',
+                },
+                {
+                  path: '/home/search',
+                  name: '搜索',
+                  hideInMenu: true,
+                  locale: 'menu.home.search',
+                },
+              ],
+            },
+            {
+              path: '/data_hui',
+              name: '汇总数据',
+              locale: 'menu.data_hui',
+              children: [
+                {
+                  collapsed: true,
+                  menuName: '域买家维度交易',
+                  name: '域买家维度交易',
+                  children: [
+                    {
+                      id: 2,
+                      name: '月表',
+                      path: '/data_hui2',
+                    },
+                    {
+                      name: '日表',
+                      path: '/data_hui3?tableName=adm_rk_cr_tb_trv_byr_ds&tableSchema=alifin_odps_birisk',
+                    },
+                  ],
+                },
+                {
+                  name: '维度交易',
+                  path: '/',
+                  children: [
+                    {
+                      name: '月表',
+                      path: '/data_hui4',
+                    },
+                    {
+                      name: '日表',
+                      key: 'tableName=adm_rk_cr_tb_trv_byr_ds&tableSchema=alifin_odps_birisk',
+                      path: '/data_hui5',
+                    },
+                  ],
+                },
+              ],
+            },
+          ]}
+        />
+      );
+    };
+    const html = mount(<Demo />);
+    await waitForComponentToPaint(html);
+
+    expect(html.find('li.ant-menu-submenu').length).toBe(3);
+    expect(html.find('li.ant-menu-submenu-open').length).toBe(3);
+
+    act(() => {
+      html.find('li.ant-pro-sider-collapsed-button').simulate('click');
+    });
+    await waitForComponentToPaint(html, 100);
+    expect(html.find('li.ant-menu-submenu-open').length).toBe(0);
+
+    act(() => {
+      html.find('li.ant-pro-sider-collapsed-button').simulate('click');
+    });
+    await waitForComponentToPaint(html, 100);
+    expect(html.find('li.ant-menu-submenu-open').length).toBe(3);
+  });
 });
