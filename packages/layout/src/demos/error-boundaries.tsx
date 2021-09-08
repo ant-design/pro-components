41b7184abd9bbdf5de50e657da0d7ee7d722a4dd
愚道 <tingzhao.ytz@antfin.com>
2021-09-08 11:44:42 +0800

    chore: update ErrorBoundary custom boundary demo

--- packages/layout/src/demos/error-boundaries.tsx
+++ packages/layout/src/demos/error-boundaries.tsx
@@ -1,71 +1,90 @@
 ﻿import React, { useState } from 'react';
+import type { ErrorInfo } from 'react';
 import { Button, Result } from 'antd';
 
 import ProLayout, { PageContainer } from '@ant-design/pro-layout';
 
-const CustomErrorPage = () => {
-  return (
-    <Result
-      status="warning"
-      style={{
-        height: '100%',
-        background: '#fff',
-      }}
-      title="错误处理"
-      extra={
-        <>
-          <div
-            style={{
-              maxWidth: 620,
-              textAlign: 'left',
-              backgroundColor: 'rgba(255,229,100,0.3)',
-              borderLeftColor: '#ffe564',
-              borderLeftWidth: '9px',
-              borderLeftStyle: 'solid',
-              padding: '20px 45px 20px 26px',
-              margin: 'auto',
-              marginBottom: '30px',
-              marginTop: '20px',
-            }}
-          >
-            <p>注意</p>
-            <p>
-              错误边界<strong>无法</strong>捕获以下场景中产生的错误：
-            </p>
-            <ul
-              style={{
-                listStyle: 'none',
-              }}
-            >
-              <li>
-                事件处理（
-                <a href="https://zh-hans.reactjs.org/docs/error-boundaries.html#how-about-event-handlers#how-about-event-handlers">
-                  了解更多
-                </a>
-                ）
-              </li>
-              <li>
-                异步代码（例如 <code>setTimeout</code> 或 <code>requestAnimationFrame</code>{' '}
-                回调函数）
-              </li>
-              <li>服务端渲染</li>
-              <li>它自身抛出来的错误（并非它的子组件）</li>
-            </ul>
-          </div>
-          <Button
-            danger
-            type="primary"
-            onClick={() => {
-              window.location.reload();
-            }}
-          >
-            刷新页面
-          </Button>
-        </>
-      }
-    />
-  );
-};
+class CustomBoundary extends React.Component<{}, { hasError: boolean; errorInfo: string }> {
+  state = { hasError: false, errorInfo: '' };
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, errorInfo: error.message };
+  }
+
+  componentDidCatch(error: any, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    // eslint-disable-next-line no-console
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <Result
+          status="warning"
+          style={{
+            height: '100%',
+            background: '#fff',
+          }}
+          title="错误处理"
+          extra={
+            <>
+              <div
+                style={{
+                  maxWidth: 620,
+                  textAlign: 'left',
+                  backgroundColor: 'rgba(255,229,100,0.3)',
+                  borderLeftColor: '#ffe564',
+                  borderLeftWidth: '9px',
+                  borderLeftStyle: 'solid',
+                  padding: '20px 45px 20px 26px',
+                  margin: 'auto',
+                  marginBottom: '30px',
+                  marginTop: '20px',
+                }}
+              >
+                <p>注意</p>
+                <p>
+                  错误边界<strong>无法</strong>捕获以下场景中产生的错误：
+                </p>
+                <ul
+                  style={{
+                    listStyle: 'none',
+                  }}
+                >
+                  <li>
+                    事件处理（
+                    <a href="https://zh-hans.reactjs.org/docs/error-boundaries.html#how-about-event-handlers#how-about-event-handlers">
+                      了解更多
+                    </a>
+                    ）
+                  </li>
+                  <li>
+                    异步代码（例如 <code>setTimeout</code> 或 <code>requestAnimationFrame</code>{' '}
+                    回调函数）
+                  </li>
+                  <li>服务端渲染</li>
+                  <li>它自身抛出来的错误（并非它的子组件）</li>
+                </ul>
+              </div>
+              <Button
+                danger
+                type="primary"
+                onClick={() => {
+                  window.location.reload();
+                }}
+              >
+                刷新页面
+              </Button>
+            </>
+          }
+        />
+      );
+    }
+    return this.props.children;
+  }
+}
 
 const ErrorTriggerTestPage = () => {
   const [error, setError] = useState<boolean>(false);
@@ -118,7 +137,7 @@ export default () => {
             {dom}
           </a>
         )}
-        ErrorBoundary={pathname === '/custom' ? CustomErrorPage : undefined}
+        ErrorBoundary={pathname === '/custom' ? CustomBoundary : undefined}
       >
         <PageContainer>
           <ErrorTriggerTestPage />
