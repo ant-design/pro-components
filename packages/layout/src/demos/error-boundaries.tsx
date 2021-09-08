61a4717c98844afa078549efa40aadbf67261201
愚指导 <yutingzhao1991@sina.com>
2021-09-08 21:39:48 +0800

    chore: update ErrorBoundary demo for use real ErrorBoundary (#3614)
    * chore: update ErrorBoundary demo for use real ErrorBoundary
    
    * chore: update ErrorBoundary custom boundary demo
    
    * chore: update snapshots for test

--- packages/layout/src/demos/error-boundaries.tsx
+++ packages/layout/src/demos/error-boundaries.tsx
@@ -1,76 +1,110 @@
 ﻿import React, { useState } from 'react';
+import type { ErrorInfo } from 'react';
 import { Button, Result } from 'antd';
 
 import ProLayout, { PageContainer } from '@ant-design/pro-layout';
-import defaultProps from './_defaultProps';
 
-const ErrorPage = () => {
-  const [error, setError] = useState<boolean>(false);
+class CustomBoundary extends React.Component<{}, { hasError: boolean; errorInfo: string }> {
+  state = { hasError: false, errorInfo: '' };
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, errorInfo: error.message };
+  }
+
+  componentDidCatch(error: any, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    // eslint-disable-next-line no-console
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <Result
+          status="warning"
+          style={{
+            height: '100%',
+            background: '#fff',
+          }}
+          title="错误处理"
+          extra={
+            <>
+              <div
+                style={{
+                  maxWidth: 620,
+                  textAlign: 'left',
+                  backgroundColor: 'rgba(255,229,100,0.3)',
+                  borderLeftColor: '#ffe564',
+                  borderLeftWidth: '9px',
+                  borderLeftStyle: 'solid',
+                  padding: '20px 45px 20px 26px',
+                  margin: 'auto',
+                  marginBottom: '30px',
+                  marginTop: '20px',
+                }}
+              >
+                <p>注意</p>
+                <p>
+                  错误边界<strong>无法</strong>捕获以下场景中产生的错误：
+                </p>
+                <ul
+                  style={{
+                    listStyle: 'none',
+                  }}
+                >
+                  <li>
+                    事件处理（
+                    <a href="https://zh-hans.reactjs.org/docs/error-boundaries.html#how-about-event-handlers#how-about-event-handlers">
+                      了解更多
+                    </a>
+                    ）
+                  </li>
+                  <li>
+                    异步代码（例如 <code>setTimeout</code> 或 <code>requestAnimationFrame</code>{' '}
+                    回调函数）
+                  </li>
+                  <li>服务端渲染</li>
+                  <li>它自身抛出来的错误（并非它的子组件）</li>
+                </ul>
+              </div>
+              <Button
+                danger
+                type="primary"
+                onClick={() => {
+                  window.location.reload();
+                }}
+              >
+                刷新页面
+              </Button>
+            </>
+          }
+        />
+      );
+    }
+    return this.props.children;
+  }
+}
+
+const ErrorTriggerTestPage = () => {
+  // default to throw error for snapshot test
+  const [error, setError] = useState<boolean>(true);
   if (error) throw new Error('渲染发生了错误');
   return (
-    <Result
-      status="warning"
-      style={{
-        height: '100%',
-        background: '#fff',
+    <Button
+      danger
+      type="primary"
+      onClick={() => {
+        setError(true);
       }}
-      title="错误处理"
-      extra={
-        <>
-          <div
-            style={{
-              maxWidth: 620,
-              textAlign: 'left',
-              backgroundColor: 'rgba(255,229,100,0.3)',
-              borderLeftColor: '#ffe564',
-              borderLeftWidth: '9px',
-              borderLeftStyle: 'solid',
-              padding: '20px 45px 20px 26px',
-              margin: 'auto',
-              marginBottom: '30px',
-              marginTop: '20px',
-            }}
-          >
-            <p>注意</p>
-            <p>
-              错误边界<strong>无法</strong>捕获以下场景中产生的错误：
-            </p>
-            <ul
-              style={{
-                listStyle: 'none',
-              }}
-            >
-              <li>
-                事件处理（
-                <a href="https://zh-hans.reactjs.org/docs/error-boundaries.html#how-about-event-handlers#how-about-event-handlers">
-                  了解更多
-                </a>
-                ）
-              </li>
-              <li>
-                异步代码（例如 <code>setTimeout</code> 或 <code>requestAnimationFrame</code>{' '}
-                回调函数）
-              </li>
-              <li>服务端渲染</li>
-              <li>它自身抛出来的错误（并非它的子组件）</li>
-            </ul>
-          </div>
-          <Button
-            danger
-            type="primary"
-            onClick={() => {
-              setError(true);
-            }}
-          >
-            触发错误
-          </Button>
-        </>
-      }
-    />
+    >
+      触发错误
+    </Button>
   );
 };
 
 export default () => {
+  const [pathname, setPathname] = useState('/default');
   return (
     <div
       id="test-pro-layout"
@@ -79,13 +113,35 @@ export default () => {
       }}
     >
       <ProLayout
-        {...defaultProps}
-        location={{
-          pathname: '/welcome',
+        route={{
+          path: '/',
+          routes: [
+            {
+              path: '/default',
+              name: '默认错误',
+            },
+            {
+              path: '/custom',
+              name: '自定义错误',
+            },
+          ],
         }}
+        location={{
+          pathname,
+        }}
+        menuItemRender={(item, dom) => (
+          <a
+            onClick={() => {
+              setPathname(item.path || '/welcome');
+            }}
+          >
+            {dom}
+          </a>
+        )}
+        ErrorBoundary={pathname === '/custom' ? CustomBoundary : undefined}
       >
         <PageContainer>
-          <ErrorPage />
+          <ErrorTriggerTestPage />
         </PageContainer>
       </ProLayout>
     </div>
