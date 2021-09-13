33b63b149f2bf229b49fb93961ad0f8a14198dd4
Bohun Tak <bhtak@uangel.com>
2021-09-13 16:21:08 +0900

    🌎 locale:add ko-KR locale (#3652)
    * add ko_KR
    
    * feat(compiler): add 'ko_KR' locale
    
    * add ko_KR
    
    * add koKR locale
    
    * change translation
    
    * change translation

--- packages/layout/src/locales/index.ts
+++ packages/layout/src/locales/index.ts
@@ -3,12 +3,14 @@ import zhLocal from './zh-CN';
 import zhTWLocal from './zh-TW';
 import enUSLocal from './en-US';
 import itITLocal from './it-IT';
+import koKRLocal from './ko-KR';
 
 const locales = {
   'zh-CN': zhLocal,
   'zh-TW': zhTWLocal,
   'en-US': enUSLocal,
   'it-IT': itITLocal,
+  'ko-KR': koKRLocal,
 };
 
 type GLocaleWindow = {
