93439de39bc14e60c3abf114648a13d2e51adfeb
chenshuai2144 <qixian.cs@outlook.com>
2021-09-18 15:25:45 +0800

    fix test

--- packages/utils/src/merge/index.ts
+++ packages/utils/src/merge/index.ts
@@ -1,9 +1,9 @@
 ﻿/* eslint-disable prefer-rest-params */
 const merge = <T>(...rest: any[]): T => {
   const obj = {};
-  let i = 0;
   const il = rest.length;
   let key;
+  let i = 0;
   for (; i < il; i += 1) {
     // eslint-disable-next-line no-restricted-syntax
     for (key in rest[i]) {
