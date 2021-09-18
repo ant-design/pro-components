2e549e1e16060730d38793001c134c42aa1fafa9
chenshuai2144 <qixian.cs@outlook.com>
2021-09-18 15:21:34 +0800

    fix(table): use new merge funtion

---
+++ packages/utils/src/merge/index.ts
@@ -0,0 +1,18 @@
+﻿/* eslint-disable prefer-rest-params */
+const merge = <T>(...rest: any[]): T => {
+  const obj = {};
+  let i = 0;
+  const il = rest.length;
+  let key;
+  for (; i < il; i += 1) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (key in rest[i]) {
+      if (rest[i].hasOwnProperty(key)) {
+        obj[key] = rest[i][key];
+      }
+    }
+  }
+  return obj as T;
+};
+
+export { merge };
