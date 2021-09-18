91c7c8de355a1f967a144148b303b1d25ce5bf5e
chenshuai2144 <qixian.cs@outlook.com>
2021-09-18 12:22:53 +0800

    fix test

--- scripts/checkDeps.js
+++ scripts/checkDeps.js
@@ -141,7 +141,7 @@ const checkDeps = ({ cwd }) => {
       const result = await forEachFile(source, path).catch(() => {});
       if (result.success === false) {
         console.log(`😂 ${path} 发现了错误：\n ${result.message}`);
-        throw new Error(`😂 ${path} 发现了错误：\n ${result.message}`);
+        process.exit(2);
       }
     }
   });
