cdad0df900afab5e3ffbbf211726ed8a6974c780
Derrick <547791743@qq.com>
2021-10-20 17:27:03 +0800

    fix: type

--- packages/utils/src/components/FilterDropdown/index.tsx
+++ packages/utils/src/components/FilterDropdown/index.tsx
@@ -8,7 +8,7 @@ export type FooterRender =
   | ((
       onConfirm?: (e?: React.MouseEvent) => void,
       onClear?: (e?: React.MouseEvent) => void,
-    ) => JSX.Element)
+    ) => React.ReactNode)
   | false;
 
 export type DropdownProps = {
