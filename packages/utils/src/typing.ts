28192b2b4d52a67b03086835d8f6aa5a24ff0056
陈帅 <qixian.cs@outlook.com>
2021-11-02 14:21:53 +0800

    chore: fix ts error

--- packages/utils/src/typing.ts
+++ packages/utils/src/typing.ts
@@ -170,7 +170,7 @@ export type ProSchema<
   Entity = Record<string, any>,
   ExtraProps = unknown,
   ComponentsType = ProSchemaComponentTypes,
-  ValueType = string,
+  ValueType = 'text',
 > = {
   /** @name 确定这个列的唯一值,一般用于 dataIndex 重复的情况 */
   key?: React.Key;
