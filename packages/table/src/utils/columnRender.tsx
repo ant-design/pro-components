04d6a6c94aaf1597ec5bbdb9135fdacbdca9640a
陈帅 <qixian.cs@outlook.com>
2021-11-17 18:03:41 +0800

    fix(descriptions): unuse protable (#4062)
    * fix(descriptions): unuse protable
    
    * merge

--- packages/table/src/utils/columnRender.tsx
+++ packages/table/src/utils/columnRender.tsx
@@ -1,12 +1,12 @@
 ﻿import React from 'react';
-import { Space, Form, Typography } from 'antd';
+import { Space, Form } from 'antd';
 import type {
   ProFieldValueType,
   ProSchemaComponentTypes,
   ProTableEditableFnType,
   UseEditableUtilType,
 } from '@ant-design/pro-utils';
-import { isNil, isDeepEqualReact } from '@ant-design/pro-utils';
+import { isNil, genCopyable, isDeepEqualReact } from '@ant-design/pro-utils';
 import type { ProFieldEmptyText } from '@ant-design/pro-field';
 import cellRenderToFromItem from './cellRenderToFromItem';
 import { LabelIconTip } from '@ant-design/pro-utils';
@@ -53,59 +53,6 @@ function isEditableCell<T>(
   }
   return editable?.(text, rowData, index) === false;
 }
-
-const isNeedTranText = (item: ProColumns<any>): boolean => {
-  if (item?.valueType?.toString().startsWith('date')) {
-    return true;
-  }
-  if (item?.valueType === 'select' || item?.valueEnum) {
-    return true;
-  }
-  return false;
-};
-
-/**
- * 生成 Copyable 或 Ellipsis 的 dom
- *
- * @param dom
- * @param item
- * @param text
- */
-export const genCopyable = (dom: React.ReactNode, item: ProColumns<any>, text: string) => {
-  if (item.copyable || item.ellipsis) {
-    const copyable =
-      item.copyable && text
-        ? {
-            text,
-            tooltips: ['', ''],
-          }
-        : undefined;
-
-    /** 有些 valueType 需要设置copy的为string */
-    const needTranText = isNeedTranText(item);
-    const ellipsis =
-      item.ellipsis && text
-        ? {
-            tooltip: needTranText ? <div className="pro-table-tooltip-text">{dom}</div> : text,
-          }
-        : false;
-    return (
-      <Typography.Text
-        style={{
-          width: '100%',
-          margin: 0,
-          padding: 0,
-        }}
-        title=""
-        copyable={copyable}
-        ellipsis={ellipsis}
-      >
-        {dom}
-      </Typography.Text>
-    );
-  }
-  return dom;
-};
 
 /**
  * 默认的 filter 方法
