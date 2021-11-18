5ae83b3d975a31e9287b2401639aa3ed22c8b532
陈帅 <qixian.cs@outlook.com>
2021-11-18 10:14:02 +0800

    merge

--- packages/table/src/utils/columnRender.tsx
+++ packages/table/src/utils/columnRender.tsx
@@ -54,67 +54,6 @@ function isEditableCell<T>(
   return editable?.(text, rowData, index) === false;
 }
 
-const isNeedTranText = (item: ProColumns<any>): boolean => {
-  if (item?.valueType?.toString().startsWith('date')) {
-    return true;
-  }
-  if (item?.valueType === 'select' || item?.valueEnum) {
-    return true;
-  }
-  return false;
-};
-
-/**
- * 生成 Copyable 或 Ellipsis 的 dom
- *
- * @param dom
- * @param item
- * @param text
- */
-export const genCopyable = (dom: React.ReactNode, item: ProColumns<any>, text: string) => {
-  if (item.copyable || item.ellipsis) {
-    let domText = dom;
-
-    const copyable =
-      item.copyable && text
-        ? {
-            text,
-            tooltips: ['', ''],
-          }
-        : undefined;
-
-    /** 有些 valueType 需要设置copy的为string */
-    const needTranText = isNeedTranText(item);
-
-    /** 剔除警告 https://github.com/ant-design/ant-design/blob/master/components/typography/Base.tsx#L329 */
-    if (React.isValidElement(dom)) {
-      domText = dom.props?.text || dom;
-    }
-
-    const ellipsis =
-      item.ellipsis && text
-        ? {
-            tooltip: needTranText ? <div className="pro-table-tooltip-text">{dom}</div> : text,
-          }
-        : false;
-    return (
-      <Typography.Text
-        style={{
-          width: '100%',
-          margin: 0,
-          padding: 0,
-        }}
-        title=""
-        copyable={copyable}
-        ellipsis={ellipsis}
-      >
-        {domText}
-      </Typography.Text>
-    );
-  }
-  return dom;
-};
-
 /**
  * 默认的 filter 方法
  *
