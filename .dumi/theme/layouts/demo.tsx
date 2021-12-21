2256f78381e382643df5115f33f1151f21a4e897
chenshuai2144 <qixian.cs@outlook.com>
2021-12-21 16:23:32 +0800

    docs: support ssr

--- .dumi/theme/layouts/demo.tsx
+++ .dumi/theme/layouts/demo.tsx
@@ -1,11 +1,15 @@
 ﻿import React from 'react';
-
 import { SettingDrawer } from '@ant-design/pro-layout';
+import { isBrowser } from 'umi';
 
 export default ({ children, location, ...rest }) => {
+  if (!isBrowser()) {
+    return children;
+  }
   if (location.pathname.startsWith('/~demos/layout')) {
     return children;
   }
+
   return (
     <div
       style={{
