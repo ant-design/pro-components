f3347c0b0ca1ea01889f17ef9099e4fcbf64b208
期贤 <qixian.cs@antgroup.com>
2022-01-10 21:55:12 +0800

    docs: fix docs
    close https://github.com/ant-design/pro-components/issues/4438

--- docs/demos/valueType.tsx
+++ docs/demos/valueType.tsx
@@ -110,46 +110,49 @@ export default () => {
                 valueType,
                 title: '编辑器',
                 dataIndex: valueType || 'text',
-                valueEnum,
-                fieldProps: {
-                  multiple: true,
-                  options: [
-                    {
-                      title: 'Node1',
-                      value: '0-0',
-                      key: '0-0',
-                      children: [
-                        {
-                          title: 'Child Node1',
-                          value: '0-0-0',
-                          key: '0-0-0',
-                        },
-                      ],
-                    },
-                    {
-                      title: 'Node2',
-                      value: '0-1',
-                      key: '0-1',
-                      children: [
-                        {
-                          title: 'Child Node3',
-                          value: '0-1-0',
-                          key: '0-1-0',
-                        },
-                        {
-                          title: 'Child Node4',
-                          value: '0-1-1',
-                          key: '0-1-1',
-                        },
-                        {
-                          title: 'Child Node5',
-                          value: '0-1-2',
-                          key: '0-1-2',
-                        },
-                      ],
-                    },
-                  ],
-                },
+                valueEnum: valueType === 'select' ? valueEnum : undefined,
+                fieldProps:
+                  valueType === 'select'
+                    ? {
+                        multiple: true,
+                        options: [
+                          {
+                            title: 'Node1',
+                            value: '0-0',
+                            key: '0-0',
+                            children: [
+                              {
+                                title: 'Child Node1',
+                                value: '0-0-0',
+                                key: '0-0-0',
+                              },
+                            ],
+                          },
+                          {
+                            title: 'Node2',
+                            value: '0-1',
+                            key: '0-1',
+                            children: [
+                              {
+                                title: 'Child Node3',
+                                value: '0-1-0',
+                                key: '0-1-0',
+                              },
+                              {
+                                title: 'Child Node4',
+                                value: '0-1-1',
+                                key: '0-1-1',
+                              },
+                              {
+                                title: 'Child Node5',
+                                value: '0-1-2',
+                                key: '0-1-2',
+                              },
+                            ],
+                          },
+                        ],
+                      }
+                    : undefined,
                 formItemProps: {
                   rules: [
                     {
