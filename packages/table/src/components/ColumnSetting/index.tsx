2541951477e067ae90ea9186c5e677f9b6e521ce
sarufarr <sarufarr@gmail.com>
2022-02-23 10:52:28 +0800

    feat(table): support antd Table extra column (#9604)

--- packages/table/src/components/ColumnSetting/index.tsx
+++ packages/table/src/components/ColumnSetting/index.tsx
@@ -228,7 +228,7 @@ const CheckboxList: React.FC<{
 };
 
 const GroupCheckboxList: React.FC<{
-  localColumns: (ProColumns<any> & { index?: number })[];
+  localColumns: (ProColumns<any> & { index?: number; isExtraColumns?: boolean })[];
   className?: string;
   draggable: boolean;
   checkable: boolean;
@@ -241,6 +241,9 @@ const GroupCheckboxList: React.FC<{
   localColumns.forEach((item) => {
     /** 不在 setting 中展示的 */
     if (item.hideInSetting) {
+      return;
+    }
+    if (item.isExtraColumns) {
       return;
     }
     const { fixed } = item;
