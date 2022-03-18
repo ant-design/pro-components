8d2b642d0ddda5c97ce6981933365750e4563439
dengqing <1247748612@qq.com>
2022-03-18 11:40:58 +0800

    docs: ✏️ update demo and doc (#4870)
    Co-authored-by: dengqing <dengqing0821@gmail.com>

--- packages/form/src/components/SchemaForm/demos/dynamic-rerender.tsx
+++ packages/form/src/components/SchemaForm/demos/dynamic-rerender.tsx
@@ -81,15 +81,27 @@ const columns: ProFormColumnsType<DataItem>[] = [
     },
   },
   {
-    title: 'title为hidden时隐藏',
-    dataIndex: 'hidden',
-    valueType: 'date',
-    renderFormItem: (_, __, form) => {
-      if (form.getFieldValue('title') === 'hidden') {
-        return false;
-      } else {
-        return <Input />;
-      }
+    valueType: 'dependency',
+    fieldProps: {
+      name: ['title'],
+    },
+    columns: ({ title }) => {
+      return title !== 'hidden'
+        ? [
+            {
+              title: 'title为hidden时隐藏',
+              dataIndex: 'hidden',
+              valueType: 'date',
+              renderFormItem: (_, __, form) => {
+                if (form.getFieldValue('title') === 'hidden') {
+                  return false;
+                } else {
+                  return <Input />;
+                }
+              },
+            },
+          ]
+        : [];
     },
   },
   {
