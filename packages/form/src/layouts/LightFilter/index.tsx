7aa035234d2dde2afe22bf9c245f87fa1abba3ef
陈帅 <qixian.cs@outlook.com>
2022-04-14 12:44:04 +0800

    docs: add more docs

--- packages/form/src/layouts/LightFilter/index.tsx
+++ packages/form/src/layouts/LightFilter/index.tsx
@@ -21,10 +21,33 @@ import type { LightFilterFooterRender, Placement } from '../../interface';
 
 export type LightFilterProps<T> = {
   collapse?: boolean;
+  /**
+   * @name 收起的label dom
+   *
+   * @example collapseLabel={"收起"}
+   */
   collapseLabel?: React.ReactNode;
+  /**
+   * @name 是否有边框
+   */
   bordered?: boolean;
+  /**
+   * @name 忽略rules，一般而言 LightFilter 应该不支持rules，默认是 false。
+   */
   ignoreRules?: boolean;
+
+  /**
+   * @name 自定义 footerRender
+   *
+   * @example 自定义清除
+   * footerRender={(onConfirm,onClear)=>{  return <a onClick={onClear}>清除</a> })}
+   */
   footerRender?: LightFilterFooterRender;
+
+  /**
+   * @name 支持配置弹出的位置
+   * @default bottomLeft
+   */
   placement?: Placement;
 } & Omit<FormProps<T>, 'onFinish'> &
   CommonFormProps<T>;
