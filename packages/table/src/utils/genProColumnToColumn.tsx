544fb06845e446f5dd25d33addd7c5d9254df47c
陈帅 <qixian.cs@outlook.com>
2022-04-22 10:21:35 +0800

    update snapshot

--- packages/table/src/utils/genProColumnToColumn.tsx
+++ packages/table/src/utils/genProColumnToColumn.tsx
@@ -63,8 +63,13 @@ export function genProColumnToColumn<T>(
           ...columnProps,
         };
       }
+
+      /**
+       * 是不是展开行和多选按钮
+       */
       const isExtraColumns =
         columnProps === Table.EXPAND_COLUMN || columnProps === Table.SELECTION_COLUMN;
+
       if (isExtraColumns) {
         return {
           index: columnsIndex,
