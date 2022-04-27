5bebf95ea94d2475fb0547af327c4a10443e4b21
陈帅 <qixian.cs@outlook.com>
2022-04-27 21:36:57 +0800

    fix test

--- tests/utils/index.test.tsx
+++ tests/utils/index.test.tsx
@@ -15,10 +15,10 @@ import {
   isDeepEqualReact,
 } from '@ant-design/pro-utils';
 import { mount } from 'enzyme';
+import { render, act, fireEvent } from '@testing-library/react';
 import { Form, Input } from 'antd';
 import type { Moment } from 'moment';
 import moment from 'moment';
-import { act } from 'react-dom/test-utils';
 import { waitTime, waitForComponentToPaint } from '../util';
 import isDropdownValueType from '../../packages/utils/src/isDropdownValueType/index';
 import { CodeFilled } from '@ant-design/icons';
@@ -388,7 +388,7 @@ describe('utils', () => {
       numberRequired: '必须包含数字',
       alphaRequired: '必须包含字母',
     };
-    const html = mount(
+    const html = render(
       <Form>
         <InlineErrorFormItem
           errorType="popover"
@@ -413,34 +413,36 @@ describe('utils', () => {
           popoverProps={{ trigger: 'focus' }}
           name="title"
         >
-          <Input id="test" />
+          <Input id="test" role="test_input" />
         </InlineErrorFormItem>
       </Form>,
     );
 
-    act(() => {
-      html.find('input#test').simulate('focus');
+    await act(async () => {
+      (await html.findByRole('test_input')).focus();
     });
     await waitForComponentToPaint(html, 100);
-    expect(html.find('div.ant-popover').exists()).toBeFalsy();
+    expect(!!html.baseElement.querySelector('div.ant-popover')).toBeFalsy();
 
-    act(() => {
-      html.find('input#test').simulate('change', {
+    await act(async () => {
+      const dom = await html.findByRole('test_input');
+      fireEvent.change(dom!, {
         target: {
           value: '1',
         },
       });
     });
     await waitForComponentToPaint(html, 1000);
-    expect(html.find('div.ant-popover').exists()).toBeTruthy();
-    const li = html.find(
+    expect(!!html.baseElement.querySelector('div.ant-popover')).toBeTruthy();
+    const li = html.baseElement.querySelectorAll(
       'div.ant-popover .ant-popover-inner-content div.ant-form-item-explain-error',
     );
-    expect(li.exists()).toBeTruthy();
-    expect(li.at(0).text()).toBe(ruleMessage.min);
-    expect(li.at(1).text()).toBe(ruleMessage.alphaRequired);
-    act(() => {
-      html.find('input#test').simulate('change', {
+    expect(li.length > 0).toBeTruthy();
+    expect(li[0].textContent).toBe(ruleMessage.min);
+    expect(li[1].textContent).toBe(ruleMessage.alphaRequired);
+    await act(async () => {
+      const dom = await html.findByRole('test_input');
+      fireEvent.change(dom!, {
         target: {
           value: '12345678901AB',
         },
@@ -448,25 +450,31 @@ describe('utils', () => {
     });
     await waitForComponentToPaint(html, 1000);
 
-    act(() => {
-      html.find('input#test').simulate('change', {
+    await act(async () => {
+      const dom = await html.findByRole('test_input');
+      fireEvent.change(dom!, {
         target: {
           value: '.',
         },
       });
     });
     await waitForComponentToPaint(html, 1000);
-    expect(html.find('div.ant-popover.ant-popover-hidden').exists()).toBeFalsy();
+    expect(
+      html.baseElement.querySelectorAll('div.ant-popover.ant-popover-hidden').length > 0,
+    ).toBeFalsy();
 
-    act(() => {
-      html.find('input#test').simulate('change', {
+    await act(async () => {
+      const dom = await html.findByRole('test_input');
+      fireEvent.change(dom!, {
         target: {
           value: '',
         },
       });
     });
     await waitForComponentToPaint(html, 1000);
-    expect(html.find('div.ant-popover.ant-popover-hidden').exists()).toBeFalsy();
+    expect(
+      html.baseElement.querySelectorAll('div.ant-popover.ant-popover-hidden').length > 0,
+    ).toBeFalsy();
   });
 
   it('📅 transformKeySubmitValue return string', async () => {
