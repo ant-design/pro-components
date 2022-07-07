2fe76edea9acd07d1f167762ebd52f6fe7a163a2
TonyWang1993 <T_wang1993@163.com>
2022-07-07 22:32:54 +0800

    feat(layout): layout support simple app list (#5501)
    * feat: pro-layout添加新的appList样式
    
    * feat: pro-layout添加新的appList样式
    
    * fix: 修改comment
    
    * update lock
    
    * update lock
    
    * fix ts ci
    
    * fix
    
    * fix: 修改comment
    
    * fix: 交互修改
    
    * fix: conflict
    
    Co-authored-by: 伶一 <lingyi.wt@alibaba-inc.com>
    Co-authored-by: 期贤 <qixian.cs@antgroup.com>

--- packages/layout/src/components/AppsLogoComponents/simple.ts
+++ packages/layout/src/components/AppsLogoComponents/simple.ts
@@ -1,9 +1,9 @@
-﻿import { css } from '../../emotion';
+import { css } from '../../emotion';
 import type { LayoutDesignToken } from '../../ProLayoutContext';
 
 export const appContentListCss = css`
   box-sizing: content-box;
-  max-width: 656px;
+  max-width: 376px;
   margin: 0;
   padding: 0;
   list-style: none;
@@ -22,33 +22,53 @@ export const getAntdPopoverContentListCss = (prefix: string) => css`
 export const getAppContentLisItem = (designToken: LayoutDesignToken) => css`
   position: relative;
   display: inline-block;
-  width: 328px;
-  height: 72px;
-  padding: 16px;
+  width: 104px;
+  height: 104px;
+  padding: 24px;
+  margin: 8px;
   vertical-align: top;
   list-style-type: none;
   transition: transform 0.2s cubic-bezier(0.333, 0, 0, 1);
   border-radius: ${designToken.borderRadiusBase};
+
   &:hover {
     background-color: rgba(0, 0, 0, 0.03);
   }
+
   a {
     display: flex;
+    flex-direction: column;
+    align-items: center;
     height: 100%;
     font-size: 12px;
     text-decoration: none;
+    & > #avatarLogo {
+      width: 40px;
+      height: 40px;
+      margin: 0 auto;
+      color: #1890ff;
+      font-size: 22px;
+      line-height: 40px;
+      text-align: center;
+      background-color: #e8f0fb;
+      border-radius: 4px;
+    }
+
     & > img {
       width: 40px;
       height: 40px;
     }
+
     & > div {
-      margin-left: 14px;
+      margin-top: 5px;
+      margin-left: 0px;
       color: ${designToken.colorHeading};
       font-size: 14px;
       line-height: 22px;
       white-space: nowrap;
       text-overflow: ellipsis;
     }
+
     & > div > span {
       color: ${designToken.colorTextSecondary};
       font-size: 12px;
