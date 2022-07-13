199cd5c2f5c024247a60d29f5f4f40cc2bd44a77
Jeremy Lopez <jeremyandres.lopez@gmail.com>
2022-07-12 22:29:35 -0400

    fix(linting): linting

--- packages/utils/src/index.tsx
+++ packages/utils/src/index.tsx
@@ -69,7 +69,11 @@ import type {
   UseEditableType,
   UseEditableUtilType,
 } from './useEditableArray';
-import useEditableArray, { editableRowByKey, recordKeyToString, defaultActionRender } from './useEditableArray';
+import useEditableArray, {
+  defaultActionRender,
+  editableRowByKey,
+  recordKeyToString,
+} from './useEditableArray';
 import type { UseEditableMapType, UseEditableMapUtilType } from './useEditableMap';
 import useEditableMap from './useEditableMap';
 import useMountMergeState from './useMountMergeState';
@@ -153,5 +157,5 @@ export {
   useDeepCompareEffectDebounce,
   useLatest,
   useDebounceValue,
-  defaultActionRender
+  defaultActionRender,
 };
