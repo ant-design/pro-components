3eac3baab267ee0cc17b8feb364efa29b6ac7202
chenshuai2144 <qixian.cs@outlook.com>
2022-08-06 14:45:45 +0800

    增加测试

--- packages/utils/src/useMountMergeState/index.ts
+++ packages/utils/src/useMountMergeState/index.ts
@@ -1,18 +1,3 @@
 import useMergedState from 'rc-util/lib/hooks/useMergedState';
 
-type Dispatch<A> = (value: A) => void;
-
-function useMountMergeState<S>(
-  initialState: S | (() => S),
-  option?: {
-    defaultValue?: S;
-    value?: S;
-    onChange?: (value: S, prevValue: S) => void;
-    postState?: (value: S) => S;
-  },
-): [S, Dispatch<S>] {
-  const [state, setState] = useMergedState<S>(initialState, option);
-  return [state, setState];
-}
-
-export default useMountMergeState;
+export default useMergedState;
