c0fc4c5e02529aa8195cf3dbc4e5aff9cdd55877
期贤 <qixian.cs@antgroup.com>
2022-08-24 11:17:33 +0800

    增加兼容代码

--- packages/form/src/index.tsx
+++ packages/form/src/index.tsx
@@ -2,6 +2,9 @@ import { ProForm } from './layouts';
 // 兼容代码-----------
 import 'antd/es/drawer/style';
 import 'antd/es/form/style';
+import 'antd/es/modal/style';
+import 'antd/es/rate/style';
+import 'antd/es/row/style';
 import 'antd/es/steps/style';
 import 'antd/es/tabs/style';
 import 'antd/es/upload/style';
