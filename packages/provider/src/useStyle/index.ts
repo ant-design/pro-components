9a630d2aacda836010d78431dc6205e4daff519e
期贤 <qixian.cs@antgroup.com>
2022-10-21 10:25:54 +0800

    add-my-hashid

--- packages/provider/src/useStyle/index.ts
+++ packages/provider/src/useStyle/index.ts
@@ -95,6 +95,19 @@ export const operationUnit = (token: ProAliasToken): CSSObject => ({
   },
 });
 
+const hashCode = (str: string, seed = 1) => {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
 /**
  * 封装了一下 antd 的 useStyle，支持了一下antd@4
  * @param componentName {string} 组件的名字
@@ -107,6 +120,7 @@ export function useStyle(
 ): UseStyleResult {
   const { token, hashId, theme } = useToken();
   const proContext = useContext(ProProvider);
+  const proHashId = hashCode(JSON.stringify(proContext.token || {}));
 
   const { getPrefixCls } = useContext(ConfigProvider.ConfigContext);
   /**
@@ -122,7 +136,7 @@ export function useStyle(
         theme,
         token,
         hashId,
-        path: [componentName],
+        path: [componentName, `pro-${proHashId}`],
       },
       () =>
         styleFn({
