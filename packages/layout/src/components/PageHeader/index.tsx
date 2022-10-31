dffd8070f2dd61cfcbd4df6abb95666f0886ecfe
期贤 <qixian.cs@antgroup.com>
2022-10-31 16:23:31 +0800

    remove unuse code

--- packages/layout/src/components/PageHeader/index.tsx
+++ packages/layout/src/components/PageHeader/index.tsx
@@ -137,7 +137,7 @@ const renderChildren = (prefixCls: string, children: React.ReactNode, hashId: st
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
   const [compact, updateCompact] = React.useState<boolean>(false);
   const onResize = ({ width }: { width: number }) => {
-    updateCompact(width < 768, true);
+    updateCompact(width < 768);
   };
   const { getPrefixCls, direction } = React.useContext(ConfigProvider.ConfigContext);
 
