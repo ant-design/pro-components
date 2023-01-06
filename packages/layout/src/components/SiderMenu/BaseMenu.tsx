8e43d701d5977398b37305940dea0118450cb167
tchaos <wtao0410@sian.com>
2023-01-06 16:52:03 +0800

    修改变量类型
    修复因为Sider折叠导致菜单展开状态丢失的bug
    Sider关闭后（collapsed=true）会将展开的菜单收起，当Sider再次打开时（collapsed=false）会丢失原本的菜单展开状态

--- packages/layout/src/components/SiderMenu/BaseMenu.tsx
+++ packages/layout/src/components/SiderMenu/BaseMenu.tsx
@@ -552,7 +552,7 @@ const BaseMenu: React.FC<BaseMenuProps & PrivateSiderMenuProps> = (props) => {
         [`${baseClassName}-collapsed`]: props.collapsed,
       })}
       items={menuUtils.getNavMenuItems(finallyData, 0)}
-      onOpenChange={(_openKeys: string[])=> {
+      onOpenChange={(_openKeys: React.Key[])=> {
         if (!props.collapsed) {
           setOpenKeys(_openKeys)
         }
