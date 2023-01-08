c256504bfc8f44bd73714256547cbd430ce5b756
lijiawei <ljw@live.jp>
2023-01-08 19:36:19 +0800

    fix: 简化代码

--- packages/table/src/components/ColumnSetting/index.tsx
+++ packages/table/src/components/ColumnSetting/index.tsx
@@ -253,7 +253,9 @@ const CheckboxList: React.FC<{
         );
       }}
       height={listHeight}
-      treeData={treeDataConfig.list?.map(({ disabled, ...restProps }) => restProps)}
+      treeData={treeDataConfig.list?.map(
+        ({ disabled /* 不透传 disabled，使子节点禁用时也可以拖动调整顺序 */, ...config }) => config,
+      )}
     />
   );
   return (
