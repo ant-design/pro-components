fd6aaed341337cd9804235b3a4accab16f4d16a3
VI <ljw@live.jp>
2023-01-10 12:03:01 +0800

    fix(Table): 列配置子项 disable 时，“固定”按钮点击无效 (#6475)
    Co-authored-by: lijiawei <97830321+mobiliabrus@users.noreply.github.com>

--- packages/table/src/components/ColumnSetting/index.tsx
+++ packages/table/src/components/ColumnSetting/index.tsx
@@ -47,8 +47,6 @@ const ToolTipIcon: React.FC<{
           e.stopPropagation();
           e.preventDefault();
           const config = columnsMap[columnKey] || {};
-          const disableIcon = typeof config.disable === 'boolean' && config.disable;
-          if (disableIcon) return;
           const columnKeyMap = {
             ...columnsMap,
             [columnKey]: { ...config, fixed } as ColumnsState,
