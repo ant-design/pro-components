d897e90a68665d812f25226133eda0b9df91a962
期贤 <qixian.cs@antgroup.com>
2023-02-08 17:19:47 +0800

    chore: update snapshot

--- scripts/release.js
+++ scripts/release.js
@@ -1,11 +1,11 @@
-const { utils } = require('umi');
+const { yParser, chalk } = require('@umijs/utils');
 const { join } = require('path');
 const exec = require('./utils/exec');
+const execa = require('execa');
 const inquirer = require('inquirer');
 const getPackages = require('./utils/getPackages');
 const isNextVersion = require('./utils/isNextVersion');
 
-const { yParser, execa, chalk } = utils;
 const cwd = process.cwd();
 const args = yParser(process.argv);
 const lernaCli = require.resolve('lerna/cli');
