f1b8b879467934bc8a88b88184f6928842fe8337
期贤 <qixian.cs@antgroup.com>
2023-02-10 14:13:43 +0800

    chore: fix lib build path has es path error

--- scripts/replaceLib.js
+++ scripts/replaceLib.js
@@ -1,16 +1,16 @@
 const { join, dirname } = require('path');
 const fs = require('fs');
 
-const cwd = process.cwd();
-
 function replacePath(path) {
   if (path.node.source && /\/es\//.test(path.node.source.value)) {
     const esModule = path.node.source.value.replace('/es/', '/lib/');
-    const esPath = dirname(join(cwd, `node_modules/${esModule}`));
-    if (fs.existsSync(esPath)) {
-      console.log(`[es build] replace ${path.node.source.value} with ${esModule}`);
-      path.node.source.value = esModule;
-    }
+    const esPath = dirname(join(__dirname, '../', `node_modules/${esModule}`));
+    console.log(path.node.source.value, esPath);
+    try {
+      if (require.resolve(esPath)) {
+        path.node.source.value = esModule;
+      }
+    } catch (error) {}
   }
 }
 
