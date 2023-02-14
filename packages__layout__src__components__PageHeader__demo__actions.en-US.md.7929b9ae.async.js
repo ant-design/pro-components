6487588d118fb9f0713c1cf19778a839e7fe838b
期贤 <qixian.cs@antgroup.com>
2023-02-14 14:20:18 +0800

    Updates

---
+++ packages__layout__src__components__PageHeader__demo__actions.en-US.md.7929b9ae.async.js
@@ -0,0 +1,5 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[3598],{48918:function(m,s,_){_.r(s);var a=_(68e3),u=_(4923),l=_(28788),r=_(5815),d=_(4661),h=_(86658),n=_(24344),c=_(50959),e=_(11527);function t(){var i=(0,n.eL)(),o=i.texts;return(0,e.jsx)(n.dY,{children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h2",{id:"various-forms-of-pageheader",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#various-forms-of-pageheader",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Various forms of PageHeader"]}),(0,e.jsx)("p",{children:o[0].value}),(0,e.jsx)(d.Z,{lang:"tsx",children:o[1].value}),(0,e.jsx)("style",{dangerouslySetInnerHTML:{__html:`
+tr:last-child td {
+  padding-bottom: 0;
+}
+`}})]})})})}s.default=t}}]);
