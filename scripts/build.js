bee8c80d7ecbb89a4a3163c88d9e8cc812894fed
期贤 <qixian.cs@outlook.com>
2023-02-28 20:37:58 +0800

    docs: add changelog

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,8 @@
 ﻿const execa = require('execa');
 const join = require('path').join;
+const slash = require('slash2');
 
-execa('pnpm', ['--filter', './packages/**', 'build'], {
-  cwd: join(__dirname, '..'),
+execa('pnpm', ['--filter', slash('./packages/**'), 'build'], {
+  cwd: slash(join(__dirname, '..')),
   stdout: process.stdout,
 }).stdout;
