890f84280cdc0595b81d5641c9e8d2bd6d0ee500
期贤 <qixian.cs@antgroup.com>
2023-03-15 10:42:49 +0800

    Updates

---
+++ packages__layout__src__components__PageHeader__demo__responsive.en-US.md.c96857f4.async.js
@@ -0,0 +1,28 @@
+"use strict";(self.webpackChunkpro_components=self.webpackChunkpro_components||[]).push([[131],{62243:function(a,_,n){n.r(_);var m=n(76146),u=n(67692),h=n(40406),r=n(64819),o=n(40063),l=n(94910),s=n(16866),c=n(50959),e=n(11527);function i(){var d=(0,s.eL)(),t=d.texts;return(0,e.jsx)(s.dY,{children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h2",{id:"responsive",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#responsive",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"responsive"]}),(0,e.jsx)("p",{children:t[0].value}),(0,e.jsx)(o.Z,{lang:"tsx",children:t[1].value}),(0,e.jsx)("style",{dangerouslySetInnerHTML:{__html:`
+tr:last-child td {
+  padding-bottom: 0;
+}
+.ant-statistic-content {
+  font-size: 20px;
+  line-height: 28px;
+}
+#components-page-header-demo-responsive .content {
+  display: flex;
+}
+@media (max-width: 576px) {
+  #components-page-header-demo-responsive .content {
+    display: block;
+  }
+
+  #components-page-header-demo-responsive .main {
+    width: 100%;
+    margin-bottom: 12px;
+  }
+
+  #components-page-header-demo-responsive .extra {
+    width: 100%;
+    margin-left: 0;
+    text-align: left;
+  }
+}
+`}})]})})})}_.default=i}}]);
