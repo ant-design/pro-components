87071534c0b9ae0aadff65edab9cb01121bc6a55
ONLY-yours <1349021570@qq.com>
2023-03-16 20:59:18 +0800

    fix: slove queryfilter padding problem

--- packages/table/src/style/index.ts
+++ packages/table/src/style/index.ts
@@ -60,7 +60,6 @@ const genProListStyle: GenerateStyle<ProListToken> = (token) => {
       },
 
       '& &-search': {
-        padding: token.paddingMD,
         marginBlockEnd: '16px',
         background: token.colorBgContainer,
         '&-ghost': {
