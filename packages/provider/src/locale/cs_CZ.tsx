d86638bb611de6e71616462979710d72bca7d5d6
Bc. Milan Jakubec <42860182+craftedsro@users.noreply.github.com>
2023-03-18 11:29:55 +0100

    feat(components): Add translation for Slovak and Czech (#6802)
    Co-authored-by: ska266miljak <M.Counsultant@consultant.crif.com>

---
+++ packages/provider/src/locale/cs_CZ.tsx
@@ -0,0 +1,74 @@
+export default {
+  moneySymbol: 'Kč',
+  deleteThisLine: 'Smazat tento řádek',
+  copyThisLine: 'Kopírovat tento řádek',
+  form: {
+    lightFilter: {
+      more: 'Víc',
+      clear: 'Vymazat',
+      confirm: 'Potvrdit',
+      itemUnit: 'Položky',
+    },
+  },
+  tableForm: {
+    search: 'Dotaz',
+    reset: 'Resetovat',
+    submit: 'Odeslat',
+    collapsed: 'Zvětšit',
+    expand: 'Zmenšit',
+    inputPlaceholder: 'Zadejte prosím',
+    selectPlaceholder: 'Vyberte prosím',
+  },
+  alert: {
+    clear: 'Vymazat',
+    selected: 'Vybraný',
+    item: 'Položka',
+  },
+  pagination: {
+    total: {
+      range: ' ',
+      total: 'z',
+      item: 'položek',
+    },
+  },
+  tableToolBar: {
+    leftPin: 'Připnout doleva',
+    rightPin: 'Připnout doprava',
+    noPin: 'Odepnuto',
+    leftFixedTitle: 'Fixováno nalevo',
+    rightFixedTitle: 'Fixováno napravo',
+    noFixedTitle: 'Neopraveno',
+    reset: 'Resetovat',
+    columnDisplay: 'Zobrazení sloupců',
+    columnSetting: 'Nastavení',
+    fullScreen: 'Celá obrazovka',
+    exitFullScreen: 'Ukončete celou obrazovku',
+    reload: 'Obnovit',
+    density: 'Hustota',
+    densityDefault: 'Výchozí',
+    densityLarger: 'Větší',
+    densityMiddle: 'Střední',
+    densitySmall: 'Kompaktní',
+  },
+  stepsForm: {
+    next: 'Další',
+    prev: 'Předchozí',
+    submit: 'Dokončit',
+  },
+  loginForm: {
+    submitText: 'Přihlásit se',
+  },
+  editableTable: {
+    onlyOneLineEditor: 'Upravit lze pouze jeden řádek',
+    action: {
+      save: 'Uložit',
+      cancel: 'Zrušit',
+      delete: 'Vymazat',
+      add: 'přidat řádek dat',
+    },
+  },
+  switch: {
+    open: 'otevřít',
+    close: 'zavřít',
+  },
+};
