c9c48b914f2fa459c6023e633ad0e369fdd3e792
期贤 <qixian.cs@antgroup.com>
2023-04-08 21:05:04 +0800

    Updates

---
+++ packages__layout__src__components__PageHeader__demo__actions.en-US.md.b7601f0c.async.js
@@ -0,0 +1,5 @@
+"use strict";(self.webpackChunkpro_components=self.webpackChunkpro_components||[]).push([[3598],{16703:function(u,n,_){_.r(n);var a=_(54782),m=_(99604),h=_(37567),r=_(80457),o=_(79812),l=_(51510),s=_(51511),c=_(50959),e=_(11527);function i(){var d=(0,s.eL)(),t=d.texts;return(0,e.jsx)(s.dY,{children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h2",{id:"various-forms-of-pageheader",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#various-forms-of-pageheader",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Various forms of PageHeader"]}),(0,e.jsx)("p",{children:t[0].value}),(0,e.jsx)(o.Z,{lang:"tsx",children:t[1].value}),(0,e.jsx)("style",{dangerouslySetInnerHTML:{__html:`
+tr:last-child td {
+  padding-bottom: 0;
+}
+`}})]})})})}n.default=i}}]);
