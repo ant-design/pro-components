8701dd155e6f9f1db8211616c9cbb5163b7a7760
wondaywang <wondaywang@tencent.com>
2023-05-11 11:44:13 +0800

    fix: 修复内嵌form丢失form排版的问题

--- packages/form/src/components/SchemaForm/layoutType/Embed.tsx
+++ packages/form/src/components/SchemaForm/layoutType/Embed.tsx
@@ -1,3 +1,13 @@
-const Embed: React.FC<{ children: any }> = ({ children }) => <>{children}</>;
+import classNames from 'classnames';
+import { FormProps } from 'antd';
+
+const Embed: React.FC<{ children: any; layout: FormProps['layout'] }> = ({
+  children,
+  layout,
+}) => (
+  <div className={classNames('ant-form', layout && `ant-form-${layout}`)}>
+    {children}
+  </div>
+);
 
 export default Embed;
