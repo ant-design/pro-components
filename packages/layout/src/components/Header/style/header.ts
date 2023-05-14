383100fab559c500622615dd0a5d1c0bcf40acc6
期贤 <qixian.cs@antgroup.com>
2023-05-14 15:53:35 +0800

    fix: update snapshot

--- packages/layout/src/components/Header/style/header.ts
+++ packages/layout/src/components/Header/style/header.ts
@@ -23,6 +23,8 @@ const genProLayoutHeaderStyle: GenerateStyle<ProLayoutHeaderToken> = (
           token?.layout?.header?.colorBgHeader || 'rgba(255, 255, 255, 0.4)',
         WebkitBackdropFilter: 'blur(8px)',
         backdropFilter: 'blur(8px)',
+        transition:
+          'background-color 0.3s cubic-bezier(0.645, 0.045, 0.355, 1)',
         '&-fixed-header': {
           position: 'fixed',
           insetBlockStart: 0,
