d6ba262410cc2afe04ac788f1bae6fa2ea29cad1
Shinji-Li <1349021570@qq.com>
2023-05-20 12:30:57 +0800

    fix(layout): 修复 collapsedShowTitle 样式错误问题  (#7098)
    * fix: change menu show title wrong css
    
    * fix: change css

--- packages/layout/src/components/SiderMenu/style/menu.ts
+++ packages/layout/src/components/SiderMenu/style/menu.ts
@@ -100,7 +100,7 @@ const genProLayoutBaseMenuStyle: GenerateStyle<ProLayoutBaseMenuToken> = (
         },
         [`&${token.componentCls}-item-collapsed-show-title`]: {
           lineHeight: '16px',
-          height: '48px',
+          gap: 0,
           [`&${token.componentCls}-item-title-collapsed`]: {
             display: 'flex',
             [`${token.componentCls}-item-icon`]: {
