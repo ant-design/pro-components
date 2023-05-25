b3af7e3f05fed897afb8dc924190024288192361
期贤 <qixian.cs@antgroup.com>
2023-05-25 22:52:03 +0800

    feat(table): use @dnd-kit

--- packages/table/src/utils/useDragSort.tsx
+++ packages/table/src/utils/useDragSort.tsx
@@ -1,8 +1,95 @@
 import { useRefFunction } from '@ant-design/pro-utils';
 import type { TableComponents } from 'rc-table/lib/interface';
-import { SortableContainer, SortableElement } from 'react-sortable-hoc';
-import type { SortDataParams } from './index';
-import { sortData } from './index';
+import { CSS } from '@dnd-kit/utilities';
+import {
+  DndContext,
+  closestCenter,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import React from 'react';
+
+function SortableItem(props: any) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    ...props?.style,
+  };
+  const { DragHandle, dragSortKey, children, ...rest } = props;
+  if (DragHandle) {
+    let doms: React.ReactNode[] = [];
+    React.Children.forEach(children, (child: any) => {
+      if (child.key === dragSortKey) {
+        doms.push(
+          React.cloneElement(child, {
+            ...child.props,
+            children: (
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <DragHandle
+                  rowData={child?.props?.record}
+                  index={child?.props?.index}
+                  {...listeners}
+                  {...attributes}
+                />
+                {child}
+              </div>
+            ),
+          }),
+        );
+        return;
+      }
+      doms.push(child);
+    });
+    return (
+      <tr {...rest} ref={setNodeRef} style={style}>
+        {doms}
+      </tr>
+    );
+  }
+  return (
+    <tr
+      {...props}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    />
+  );
+}
+
+/**
+ * 数据排序核心逻辑
+ *
+ * @param oldIndex 原始位置
+ * @param newIndex 新位置
+ * @param data 原始数组
+ */
+export function sortData<T>(
+  oldIndex: number,
+  newIndex: number,
+  data: T[],
+): T[] | null {
+  if (oldIndex !== newIndex) {
+    return arrayMove<T>(data || [], oldIndex, newIndex);
+  }
+  return null;
+}
 
 export interface UseDragSortOptions<T> {
   dataSource?: T[];
@@ -10,44 +97,56 @@ export interface UseDragSortOptions<T> {
   dragSortKey?: string;
   components?: TableComponents<T>;
   rowKey: any;
+  DragHandle: React.FC<any>;
 }
 
-const SortableItem = SortableElement((p: any) => <tr {...p} />);
-const SortContainer = SortableContainer((p: any) => <tbody {...p} />);
+const SortContainer = (p: any) => <tbody {...p} />;
 
 export function useDragSort<T>(props: UseDragSortOptions<T>) {
-  const { dataSource = [], onDragSortEnd, dragSortKey } = props;
+  const { dataSource = [], onDragSortEnd, DragHandle, dragSortKey } = props;
 
-  // 拖拽排序相关逻辑
-  /* istanbul ignore next */
-  const handleSortEnd = useRefFunction((params: SortDataParams) => {
-    /* istanbul ignore next */
-    const newDataSource: T[] | null = sortData<T>(params, dataSource);
-    /* istanbul ignore next */
-    if (newDataSource && onDragSortEnd) {
-      /* istanbul ignore next */
-      onDragSortEnd(newDataSource);
+  const sensors = useSensors(useSensor(PointerSensor));
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    if (over?.id?.toString() && active.id !== over?.id) {
+      const newData = sortData(
+        parseInt(active.id as string),
+        parseInt(over.id as string),
+        dataSource,
+      );
+      onDragSortEnd?.(newData || []);
     }
-  });
+  }
 
   const DraggableContainer = useRefFunction((p: any) => (
-    <SortContainer
-      useDragHandle
-      disableAutoscroll
-      helperClass="row-dragging"
-      onSortEnd={handleSortEnd}
-      {...p}
-    />
+    <SortableContext
+      items={dataSource.map((_, index) => index?.toString())}
+      strategy={verticalListSortingStrategy}
+    >
+      <SortContainer {...p} />
+    </SortableContext>
   ));
 
   const DraggableBodyRow = useRefFunction((p: any) => {
     const { ...restProps } = p;
     // function findIndex base on Table rowKey props and should always be a right array index
-    const index = dataSource.findIndex(
-      (item: any) =>
-        item[props.rowKey ?? 'index'] === restProps['data-row-key'],
+    const index = dataSource
+      .findIndex(
+        (item: any) =>
+          item[props.rowKey ?? 'index'] === restProps['data-row-key'],
+      )
+      ?.toString();
+
+    return (
+      <SortableItem
+        id={index}
+        dragSortKey={dragSortKey}
+        DragHandle={DragHandle}
+        key={index}
+        {...restProps}
+      />
     );
-    return <SortableItem index={index} {...restProps} />;
   });
 
   const components: TableComponents<T> = props.components || {};
@@ -61,6 +160,17 @@ export function useDragSort<T>(props: UseDragSortOptions<T>) {
   }
 
   return {
+    DndContext: (props: any) => {
+      return (
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          {props.children}
+        </DndContext>
+      );
+    },
     components,
   };
 }
--- tests/table/dragSort.test.tsx
+++ tests/table/dragSort.test.tsx
@@ -1,6 +1,5 @@
 import { MenuOutlined } from '@ant-design/icons';
 import { DragSortTable } from '@ant-design/pro-table';
-import { sortData } from '../../packages/table/src/utils';
 import { render } from '@testing-library/react';
 
 describe('dragSort', () => {
@@ -173,32 +172,5 @@ describe('dragSort', () => {
     expect(container.querySelector('.customRender')).toMatchSnapshot();
     expect(callback).toBeCalled();
     expect(callback).toBeCalledWith('kiner', 0);
-  });
-
-  it('🔥 [dragSort] sort core', async () => {
-    type DataSourceItemStruct = {
-      id: number;
-      name: string;
-    };
-    const dataSource: DataSourceItemStruct[] = [
-      {
-        id: 1,
-        name: 'kiner',
-      },
-      {
-        id: 2,
-        name: 'WenHui Tang',
-      },
-      {
-        id: 3,
-        name: 'Kiner Tang',
-      },
-    ];
-    const newDs: DataSourceItemStruct[] | null = sortData<DataSourceItemStruct>(
-      { oldIndex: 1, newIndex: 0 },
-      dataSource,
-    );
-    expect(newDs).not.toBe(null);
-    if (newDs) expect(newDs[0].id).toBe(2);
   });
 });
