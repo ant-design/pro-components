4ca1f381d46ea40b82a36afc40314aa4c3a25777
期贤 <qixian.cs@antgroup.com>
2023-05-29 13:53:11 +0800

    up to latest verison

--- tests/layout/index.test.tsx
+++ tests/layout/index.test.tsx
@@ -132,6 +132,20 @@ describe('BasicLayout', () => {
                   },
                 ],
               },
+              {
+                path: '/home2',
+                name: '首页',
+                locale: 'menu.home2',
+                routes: [
+                  {
+                    path: '/home/overview2',
+                    name: '概述',
+                    hideInMenu: true,
+                    exact: true,
+                    locale: 'menu.home.overview',
+                  },
+                ],
+              },
             ],
           ],
         }}
@@ -158,32 +172,80 @@ describe('BasicLayout', () => {
       <ProLayout
         appList={[
           {
-            icon: '',
-            title: '区域产品解决方案平台',
-            desc: '',
-            url: 'https://ant.design',
+            title: 'UI 设计语言',
+            desc: '杭州市较知名的 UI 设计语言',
+            children: [
+              {
+                icon: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+                title: 'Ant Design',
+                desc: '杭州市较知名的 UI 设计语言',
+                url: 'https://ant.design',
+              },
+              {
+                icon: 'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
+                title: 'Pro Components',
+                desc: '专业级 UI 组件库',
+                url: 'https://procomponents.ant.design/',
+              },
+            ],
           },
           {
-            icon: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png',
-            title: 'AntV',
-            desc: '',
-            url: 'https://antv.vision/',
-            target: '_blank',
+            title: 'UI 设计语言 2组111',
+            icon: 'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
+            desc: '专业级 UI 组件库',
+            url: 'https://procomponents.ant.design/',
+            children: [
+              {
+                icon: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png',
+                title: 'AntV',
+                desc: '蚂蚁集团全新一代数据可视化解决方案',
+                url: 'https://antv.vision/',
+                target: '_blank',
+              },
+              {
+                icon: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png',
+                title: 'AntV',
+                desc: '蚂蚁集团全新一代数据可视化解决方案',
+                url: 'https://antv.vision/',
+                target: '_blank',
+              },
+            ],
           },
           {
-            icon: <div>123</div>,
-            title: 'AntV',
-            desc: '',
-            url: 'https://antv.vision/',
-            target: '_blank',
-          },
-          {
-            // @ts-ignore
-            icon: () => {},
-            title: 'AntV',
-            desc: '',
-            url: 'https://antv.vision/',
-            target: '_blank',
+            title: '待分组',
+            desc: '专业级 UI 组件库',
+            children: [
+              {
+                title: '工具',
+                desc: '知识创作与分享工具',
+                icon: 'https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg',
+                url: 'https://www.yuque.com/',
+              },
+              {
+                title: '前端应用框架',
+                desc: '插件化的企业级前端应用框架。',
+                icon: 'https://img.alicdn.com/tfs/TB1zomHwxv1gK0jSZFFXXb0sXXa-200-200.png',
+                url: 'https://umijs.org/zh-CN/docs',
+              },
+              {
+                icon: 'https://gw.alipayobjects.com/zos/bmw-prod/8a74c1d3-16f3-4719-be63-15e467a68a24/km0cv8vn_w500_h500.png',
+                title: 'qiankun',
+                desc: '可能是你见过最完善的微前端解决方案🧐',
+                url: 'https://qiankun.umijs.org/',
+              },
+              {
+                icon: 'https://gw.alipayobjects.com/zos/rmsportal/LFooOLwmxGLsltmUjTAP.svg',
+                title: 'Kitchen ',
+                desc: 'Sketch 工具集',
+                url: 'https://kitchen.alipay.com/',
+              },
+              {
+                icon: 'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
+                title: 'dumi',
+                desc: '为组件开发场景而生的文档工具',
+                url: 'https://d.umijs.org/zh-CN',
+              },
+            ],
           },
         ]}
         route={{
