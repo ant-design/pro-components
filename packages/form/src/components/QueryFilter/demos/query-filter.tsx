189f9338be78af8dbdc54cb74432844a51b2c0c6
期贤 <qixian.cs@outlook.com>
2023-07-15 22:32:35 +0800

    fix(form): support OptionType types
    close https://github.com/ant-design/pro-components/issues/6227

--- packages/form/src/components/QueryFilter/demos/query-filter.tsx
+++ packages/form/src/components/QueryFilter/demos/query-filter.tsx
@@ -1,34 +1,31 @@
 import {
-  ProFormDatePicker,
-  ProFormDateRangePicker,
-  ProFormSelect,
+  ProForm,
+  ProFormDateTimePicker,
   ProFormText,
-  QueryFilter,
 } from '@ant-design/pro-components';
 
 export default () => {
   return (
-    <QueryFilter<{
-      name: string;
-    }>
-      onFinish={async (values) => {
-        console.log(values.name);
-      }}
-    >
-      <ProFormText name="name" label="应用名称" rules={[{ required: true }]} />
-      <ProFormText name="creater" label="创建人" />
-      <ProFormSelect
-        name="sex"
-        label="性别"
-        showSearch
-        valueEnum={{
-          man: '男',
-          woman: '女',
-        }}
-      />
-      <ProFormText name="status" label="应用状态" />
-      <ProFormDatePicker name="startdate" label="响应日期" />
-      <ProFormDateRangePicker name="create" label="创建时间" colSize={3} />
-    </QueryFilter>
+    <ProForm grid={true}>
+      <ProForm.Group>
+        <ProFormDateTimePicker
+          label="下单时间"
+          colProps={{ xl: 12 }}
+          name="orderTime"
+          required
+          fieldProps={{
+            style: {
+              width: '100%',
+            },
+          }}
+        />
+        <ProFormText
+          disabled
+          colProps={{ xl: 12 }}
+          name="pay"
+          label="支付方式"
+        />
+      </ProForm.Group>
+    </ProForm>
   );
 };
