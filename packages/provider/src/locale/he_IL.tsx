42235ead5c9f3ac8d7305f4c85de5967d7ce4e08
Eden Hecht <eden@arboxapp.com>
2023-07-27 14:51:41 +0300

    hebrew support

---
+++ packages/provider/src/locale/he_IL.tsx
@@ -0,0 +1,74 @@
+export default {
+  moneySymbol: '₪',
+  deleteThisLine: 'מחק שורה זו',
+  copyThisLine: 'העתק שורה זו',
+  form: {
+    lightFilter: {
+      more: 'יותר',
+      clear: 'נקה',
+      confirm: 'אישור',
+      itemUnit: 'פריטים',
+    },
+  },
+  tableForm: {
+    search: 'חיפוש',
+    reset: 'איפוס',
+    submit: 'שלח',
+    collapsed: 'הרחב',
+    expand: 'כווץ',
+    inputPlaceholder: 'אנא הכנס',
+    selectPlaceholder: 'אנא בחר',
+  },
+  alert: {
+    clear: 'נקה',
+    selected: 'נבחר',
+    item: 'פריט',
+  },
+  pagination: {
+    total: {
+      range: ' ',
+      total: 'מתוך',
+      item: 'פריטים',
+    },
+  },
+  tableToolBar: {
+    leftPin: 'הצמד לשמאל',
+    rightPin: 'הצמד לימין',
+    noPin: 'לא מצורף',
+    leftFixedTitle: 'מוצמד לשמאל',
+    rightFixedTitle: 'מוצמד לימין',
+    noFixedTitle: 'לא מוצמד',
+    reset: 'איפוס',
+    columnDisplay: 'תצוגת עמודות',
+    columnSetting: 'הגדרות',
+    fullScreen: 'מסך מלא',
+    exitFullScreen: 'צא ממסך מלא',
+    reload: 'רענן',
+    density: 'רזולוציה',
+    densityDefault: 'ברירת מחדל',
+    densityLarger: 'גדול',
+    densityMiddle: 'בינוני',
+    densitySmall: 'קטן',
+  },
+  stepsForm: {
+    next: 'הבא',
+    prev: 'קודם',
+    submit: 'סיום',
+  },
+  loginForm: {
+    submitText: 'כניסה',
+  },
+  editableTable: {
+    onlyOneLineEditor: 'ניתן לערוך רק שורה אחת',
+    action: {
+      save: 'שמור',
+      cancel: 'ביטול',
+      delete: 'מחיקה',
+      add: 'הוסף שורת נתונים',
+    },
+  },
+  switch: {
+    open: 'פתח',
+    close: 'סגור',
+  },
+};
