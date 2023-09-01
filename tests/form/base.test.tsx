27436cc6351cfaa535d1ec9aef1f74e567ef44d7
yiyi17 <lileibao13@163.com>
2023-09-01 17:41:05 +0800

    fix(form): dependency 支持 colSize (#7611)
    * fix: dependency 支持 colSize
    
    * fix(test): ProFormTreeSelect
    
    * fix(tsc): 类型报错
    
    * fix: ProFormTreeSelect test
    
    ---------
    
    Co-authored-by: baolilei <baolilei@zhihu.com>

--- tests/form/base.test.tsx
+++ tests/form/base.test.tsx
@@ -3320,7 +3320,7 @@ describe('ProForm', () => {
       </ProForm>,
     );
 
-    await waitForWaitTime(100);
+    await waitForWaitTime(300);
 
     act(() => {
       fireEvent.change(
@@ -3334,14 +3334,14 @@ describe('ProForm', () => {
         },
       );
     });
-    await waitForWaitTime(100);
+    await waitForWaitTime(300);
     act(() => {
       fireEvent.mouseDown(
         wrapper.baseElement.querySelectorAll('.ant-select-selector')[0],
         {},
       );
     });
-    await waitForWaitTime(100);
+    await waitForWaitTime(300);
     expect(onRequest.mock.calls.length).toBe(3);
     wrapper.unmount();
   });
