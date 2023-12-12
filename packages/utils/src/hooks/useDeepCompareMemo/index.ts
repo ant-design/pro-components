6872f75a6d20ce8cac450cb85a2660585359c0aa
ONLY-yours <1349021570@qq.com>
2023-12-12 11:55:55 +0800

    fix: revert ts problem

--- packages/utils/src/hooks/useDeepCompareMemo/index.ts
+++ packages/utils/src/hooks/useDeepCompareMemo/index.ts
@@ -13,7 +13,7 @@ function useDeepCompareMemo<T>(
   factory: () => T,
   dependencies: React.DependencyList,
 ) {
-  return React.useMemo(factory, useDeepCompareMemoize(dependencies));
+  return React.useMemo(factory, useDeepCompareMemoize(dependencies) as unknown as React.DependencyList);
 }
 
 export default useDeepCompareMemo;
