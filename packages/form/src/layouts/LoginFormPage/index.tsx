cf32be9701f3fce38f79567340ec11b1ad524763
期贤 <qixian.cs@antgroup.com>
2024-01-15 15:27:42 +0800

    fix(form): fix ProFormPage submit error

--- packages/form/src/layouts/LoginFormPage/index.tsx
+++ packages/form/src/layouts/LoginFormPage/index.tsx
@@ -108,7 +108,7 @@ export function LoginFormPage<T = Record<string, any>>(
     searchConfig: {
       submitText: intl.getMessage('loginForm.submitText', '登录'),
     },
-    render: (_, dom) => dom.pop(),
+    render: (_, dom) => dom.at(0),
     ...proFormProps.submitter,
     submitButtonProps: genSubmitButtonProps(),
   } as ProFormProps['submitter'];
