114d8b4c001e9e473bd3074ad71f6124f923e854
期贤 <qixian.cs@antgroup.com>
2024-04-18 11:05:10 +0800

    chore: change build style

--- .fatherrc.base.ts
+++ .fatherrc.base.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     output: 'es',
     extraBabelPlugins: [[require.resolve('./scripts/replaceLib'), {}]],
     platform: 'browser', // 默认构建为 Browser 环境的产物
-    transformer: 'esbuild', // 默认使用 babel 以提供更好的兼容性
+    transformer: 'babel', // 默认使用 babel 以提供更好的兼容性
   },
   // 以下为 cjs 配置项启用时的默认值，有自定义需求时才需配置
   cjs: {
@@ -15,6 +15,6 @@ export default defineConfig({
     input: 'src', // 默认编译目录
     output: 'lib',
     platform: 'browser', // 默认构建为 Node.js 环境的产物
-    transformer: 'esbuild', // 默认使用 esbuild 以获得更快的构建速度
+    transformer: 'babel', // 默认使用 esbuild 以获得更快的构建速度
   },
 });
