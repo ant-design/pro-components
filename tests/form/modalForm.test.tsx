d2f9ee30c053fd95143e22a363b065e94381f03f
beautiful-boyyy <beautiful_boyyy@outlook.com>
2024-05-10 16:23:16 +0800

    fix(form): onOpenChange not fire when  opening ModalForm

--- tests/form/modalForm.test.tsx
+++ tests/form/modalForm.test.tsx
@@ -270,23 +270,22 @@ describe('ModalForm', () => {
     expect(fn).toBeCalledTimes(1); // 关闭只触发一次 onOpenChange
   });
 
-  // 如果默认就是 open 的 onOpenChange 预期应当不触发
-  // it('📦 modal open=true simulate onOpenChange', async () => {
-  //   const fn = vi.fn();
-  //   render(
-  //     <ModalForm
-  //       open
-  //       trigger={<Button id="new">新建</Button>}
-  //       onOpenChange={(visible) => fn(visible)}
-  //     >
-  //       <ProFormText name="name" />
-  //     </ModalForm>,
-  //   );
+  it('📦 modal open=true simulate onOpenChange', async () => {
+    const fn = vi.fn();
+    render(
+      <ModalForm
+        open
+        trigger={<Button id="new">新建</Button>}
+        onOpenChange={(visible) => fn(visible)}
+      >
+        <ProFormText name="name" />
+      </ModalForm>,
+    );
 
-  //   await waitFor(() => {
-  //     expect(fn).toBeCalledWith(true);
-  //   });
-  // });
+    await waitFor(() => {
+      expect(fn).toBeCalledWith(true);
+    });
+  });
 
   it('📦 reset button will simulate onOpenChange', async () => {
     const fn = vi.fn();
