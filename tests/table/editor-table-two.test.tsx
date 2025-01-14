283f981f901e0991d354633d91f09040b7b12d60
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-01-14 10:34:44 +0800

    Revert "feat: add nwsapi dependency and update related tests and snapshots"
    This reverts commit e8f9029f8e8adaf96c936b81ef3d532083c85e9e.
    
    # Conflicts:
    #	pnpm-lock.yaml

--- tests/table/editor-table-two.test.tsx
+++ tests/table/editor-table-two.test.tsx
@@ -2061,7 +2061,7 @@ describe('EditorProTable 2', () => {
     });
   });
 
-  it.skip('📝 support form rules', async () => {
+  it('📝 support form rules', async () => {
     const fn = vi.fn();
     const wrapper = render(
       <EditorProTableDemo onSave={(key, row) => fn(row.title)} />,
