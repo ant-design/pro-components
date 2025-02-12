dc24879292c5384c636dcf249efec8d0a9d5ab3e
qixian.cs <qixian.cs@antgroup.com>
2025-02-12 14:25:22 +0800

    ♻️ refactor: add UMD configuration to .fatherrc.ts for ProComponents

--- packages/components/.fatherrc.ts
+++ packages/components/.fatherrc.ts
@@ -2,4 +2,14 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   extends: '../../.fatherrc.base.ts',
+  umd: {
+    name: 'ProComponents',
+    output: 'dist',
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      '^/antd/.*': 'antd',
+      '^/dayjs/.*': 'dayjs',
+    },
+  },
 });
