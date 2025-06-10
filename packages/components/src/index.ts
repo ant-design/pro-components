75b23c07410daf2effb16e5e392de2f20acd6492
qixian.cs <qixian.cs@antgroup.com>
2025-06-10 21:06:04 +0800

    合并代码

---
+++ packages/components/src/index.ts
@@ -0,0 +1,23 @@
+import * as ProCard from './card';
+import * as ProDescriptions from './descriptions';
+import * as ProField from './field';
+import * as ProForm from './form';
+import * as ProLayout from './layout';
+import * as ProList from './list';
+import * as ProProvider from './provider';
+import * as ProSkeleton from './skeleton';
+import * as ProTable from './table';
+import * as ProUtils from './utils';
+export { version } from './version';
+export {
+  ProCard,
+  ProDescriptions,
+  ProField,
+  ProForm,
+  ProLayout,
+  ProList,
+  ProProvider,
+  ProSkeleton,
+  ProTable,
+  ProUtils,
+};
