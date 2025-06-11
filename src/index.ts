5753d1209665b2e970b94716ce30a012300dbffc
qixian.cs <qixian.cs@antgroup.com>
2025-06-11 11:06:28 +0800

    refactor: 将所有组件的文档移动到 docs 目录

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
-import * as ProCard from './card';
-import * as ProDescriptions from './descriptions';
-import * as ProField from './field';
-import * as ProForm from './form';
-import * as ProLayout from './layout';
-import * as ProList from './list';
-import * as ProProvider from './provider';
-import * as ProSkeleton from './skeleton';
-import * as ProTable from './table';
-import * as ProUtils from './utils';
+export * as ProCard from './card';
+export * as ProDescriptions from './descriptions';
+export * as ProField from './field';
+export * as ProForm from './form';
+export * as ProLayout from './layout';
+export * as ProList from './list';
+export * as ProProvider from './provider';
+export * as ProSkeleton from './skeleton';
+export * as ProTable from './table';
+export * as ProUtils from './utils';
 export { version } from './version';
-export {
-  ProCard,
-  ProDescriptions,
-  ProField,
-  ProForm,
-  ProLayout,
-  ProList,
-  ProProvider,
-  ProSkeleton,
-  ProTable,
-  ProUtils,
-};
