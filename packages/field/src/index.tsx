e0e432185a81a7c0b3225e0da46b8283d521a10e
rubbish <1956418984@qq.com>
2025-07-01 23:57:45 +0800

    feat: custom valuetypemap should not cull any props

--- packages/field/src/index.tsx
+++ packages/field/src/index.tsx
@@ -663,6 +663,7 @@ const ProFieldComponent: React.ForwardRefRenderFunction<
             ? undefined
             : (rest?.placeholder ?? fieldProps?.placeholder),
         }),
+        Object.keys(context.valueTypeMap || {})?.includes(valueType as string),
       ),
     }),
     context.valueTypeMap || {},
