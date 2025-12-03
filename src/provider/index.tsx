f532e0064bc5205082e3a1594b35ac173d40d63d
qixian <qixian.cs@antgroup.com>
2025-12-03 18:21:55 +0800

    Add prefixCls property to ConfigContext and update useStyle hook to utilize it

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -168,6 +168,7 @@ export type ConfigContextPropsType = {
   hashId?: string;
   hashed?: boolean;
   dark?: boolean;
+  prefixCls?: string;
 };
 
 /* Creating a context object with the default values. */
@@ -185,6 +186,7 @@ const ProConfigContext = React.createContext<ConfigContextPropsType>({
     antCls: '.ant',
     themeId: 0,
   },
+  prefixCls: '.ant-pro',
 });
 
 export const { Consumer: ConfigConsumer } = ProConfigContext;
@@ -267,7 +269,6 @@ const ConfigProviderContainer: React.FC<{
       (localeName && proProvide.intl?.locale === 'default'
         ? intlMap[key! as 'zh-CN']
         : proProvide.intl || intlMap[key! as 'zh-CN']);
-
     return {
       ...proProvide,
       dark: dark ?? proProvide.dark,
@@ -348,10 +349,11 @@ const ConfigProviderContainer: React.FC<{
     return {
       ...proProvideValue!,
       valueTypeMap: valueTypeMap || proProvideValue?.valueTypeMap,
-      token,
+      token: finalToken as any,
       theme: tokenContext.theme as unknown as Theme<any, any>,
       hashed,
       hashId,
+      prefixCls,
     };
   }, [
     proProvideValue,
@@ -360,6 +362,7 @@ const ConfigProviderContainer: React.FC<{
     tokenContext.theme,
     hashed,
     hashId,
+    prefixCls,
   ]);
 
   const configProviderDom = useMemo(() => {
--- src/provider/useStyle/index.ts
+++ src/provider/useStyle/index.ts
@@ -97,8 +97,11 @@ export function useStyle(
   styleFn: (token: ProAliasToken) => CSSInterpolation,
 ) {
   // eslint-disable-next-line prefer-const
-  let { token = {} as Record<string, any> as ProAliasToken, hashed } =
-    useContext(ProProvider);
+  let {
+    token = {} as Record<string, any> as ProAliasToken,
+    hashed,
+    prefixCls,
+  } = useContext(ProProvider);
 
   const { token: antdToken, hashId, theme } = antdTheme.useToken();
 
@@ -108,7 +111,9 @@ export function useStyle(
   if (!token.layout) {
     token = { ...antdToken } as any;
   }
+
   token.proComponentsCls = token.proComponentsCls ?? `.${getPrefixCls('pro')}`;
+
   token.antCls = `.${getPrefixCls()}`;
 
   // Register styles (side effect only in v2)
