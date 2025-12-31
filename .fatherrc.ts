61db24f5a99eb2b773b006492c403527d47beffd
jiaxiang <zjx9987@gmail.com>
2025-12-31 16:11:33 +0800

    chrome: Add browser targets for compatibility in .fatherrc.ts (#9364)
    * feat(config): Add browser targets for compatibility in .fatherrc.ts
    
    * chore(config): Update browser targets in .fatherrc.ts for improved compatibility

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'father';
 
+const targets = {
+  edge: 142,
+  firefox: 140,
+  chrome: 112,
+  safari: 18.5,
+  opera: 124,
+  electron: 39,
+};
+
 export default defineConfig({
   // 以下为 esm 配置项启用时的默认值，有自定义需求时才需配置
   esm: {
@@ -7,6 +16,7 @@ export default defineConfig({
     output: 'es',
     platform: 'browser', // 默认构建为 Browser 环境的产物
     transformer: 'babel', // 默认使用 babel 以提供更好的兼容性
+    targets,
   },
   // 以下为 cjs 配置项启用时的默认值，有自定义需求时才需配置
   cjs: {
@@ -14,6 +24,7 @@ export default defineConfig({
     output: 'lib',
     platform: 'browser', // 默认构建为 Node.js 环境的产物
     transformer: 'babel', // 默认使用 esbuild 以获得更快的构建速度
+    targets,
   },
   umd: {
     name: 'ProComponents',
@@ -24,5 +35,6 @@ export default defineConfig({
       '^/antd/.*': 'antd',
       '^/dayjs/.*': 'dayjs',
     },
+    targets,
   },
 });
