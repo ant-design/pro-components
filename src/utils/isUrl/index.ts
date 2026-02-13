1bc3291aae5f706b9161e0a270d30fc3aae021ff
JiaxiangZhang <zjx9987@gmail.com>
2026-02-13 14:29:35 +0800

    refactor: simplify isUrl function by combining checks for string type and URL format

--- src/utils/isUrl/index.ts
+++ src/utils/isUrl/index.ts
@@ -3,11 +3,9 @@ import isString from 'lodash-es/isString';
 /**
  * @description 判断是不是一个 url
  */
-export const isUrl = (path: unknown): boolean => {
-  if (!isString(path)) return false;
-  if (!path.startsWith('http')) {
-    return false;
-  }
+export const isUrl = (path: string): boolean => {
+  if (!isString(path) || !path.startsWith('http')) return false;
+
   try {
     new URL(path);
     return true;
