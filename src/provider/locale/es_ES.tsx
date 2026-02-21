0a153e0399912b540051a4680e569e0f56636521
smallbun <30397655+leshalv@users.noreply.github.com>
2026-02-21 22:03:40 +0800

    Update src/provider/locale/es_ES.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/provider/locale/es_ES.tsx
+++ src/provider/locale/es_ES.tsx
@@ -4,7 +4,7 @@ export default {
     lightFilter: {
       more: 'Más',
       clear: 'Limpiar',
-      reset: 'Limpiar',
+      reset: 'Restablecer',
       confirm: 'Confirmar',
       itemUnit: 'artículos',
     },
