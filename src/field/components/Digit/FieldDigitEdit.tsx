98ab21e16e2777fab2484b863a509e90dd1f98f2
jiaxiang <zjx9987@gmail.com>
2026-05-02 11:13:47 +0800

    refactor(Digit): remove proxyChange (#9610)
    * refactor(Digit): remove unused utility functions and simplify FieldDigitEdit component
    
    - Deleted the isEmptyOrWhitespace utility function as it was no longer needed.
    - Simplified the FieldDigitEdit component by removing the proxyChange logic and directly passing fieldProps to InputNumber.
    - Cleaned up imports in the index file to reflect the changes.
    
    * feat(layout): add ProFormDigit component for percentage input and adjust submission wait time
    
    - Introduced ProFormDigit for capturing percentage values with precision and an addon for '%' symbol.
    - Reduced the wait time for form submission feedback from 2000ms to 500ms for improved user experience.
    
    * fix(layout): update label for ProFormDigit component from "percent" to "Percent" for consistency

--- src/field/components/Digit/FieldDigitEdit.tsx
+++ src/field/components/Digit/FieldDigitEdit.tsx
@@ -1,13 +1,10 @@
-﻿import { omit } from '@rc-component/util';
-import { InputNumber } from 'antd';
+﻿import { InputNumber } from 'antd';
 import React from 'react';
 import type { ProFieldFC } from '../../types';
-import { isEmptyOrWhitespace } from './digitUtils';
 import type { FieldDigitProps } from './types';
 
 type Props = Parameters<ProFieldFC<FieldDigitProps>>[0] & {
   placeholderValue: string;
-  proxyChange: (value: number | string | null) => number | string | undefined;
 };
 
 export function FieldDigitEdit(props: Props, ref: React.Ref<unknown>) {
@@ -17,28 +14,13 @@ export function FieldDigitEdit(props: Props, ref: React.Ref<unknown>) {
     formItemRender,
     fieldProps,
     placeholderValue,
-    proxyChange,
   } = props;
   const dom = (
-    <InputNumber<number | string>
-      ref={ref as React.Ref<any>}
+    <InputNumber
+      ref={ref}
       min={0}
       placeholder={placeholderValue}
-      {...omit(fieldProps, ['onChange', 'onBlur'])}
-      onChange={(e) => fieldProps?.onChange?.(proxyChange(e))}
-      onBlur={(e) => {
-        const value = e.target.value;
-        if (isEmptyOrWhitespace(value)) {
-          fieldProps?.onBlur?.(e);
-          return;
-        }
-        const processedValue = proxyChange(value);
-        if (e.target && typeof processedValue === 'number') {
-          e.target.value = processedValue.toString();
-          fieldProps?.onChange?.(processedValue);
-        }
-        fieldProps?.onBlur?.(e);
-      }}
+      {...fieldProps}
     />
   );
   if (formItemRender) {
